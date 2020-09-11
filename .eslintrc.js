9104257c7281171d6916b05fdac3cc9c028fb1db
Lucas Popp <lmp122@case.edu>
2020-09-11 14:19:10 -0400

    Install and set up Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     root: true,
+    plugins: ['prettier'],
+    extends: ['eslint:recommended', 'prettier'],
     env: {
         es6: true,
         node: true,
@@ -7,5 +9,23 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-await-in-loop': 'warn',
+        'max-params': ['error', 6],
+        camelcase: [
+            'warn',
+            {
+                properties: 'never',
+            },
+        ],
+        'no-underscore-dangle': [
+            'error',
+            {
+                allowAfterThis: true,
+                allow: ['__meta__'],
+            },
+        ],
     },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    useTabs: false,
+    semi: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    allowParens: 'avoid',
+    quoteProps: 'as-needed'
+};
