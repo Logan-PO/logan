cd35c44aeb215bd5e3b9dd057d5fef6ed918de15
Lucas Popp <lmp122@case.edu>
2020-09-13 22:12:12 -0400

    Remove event from Lambda response

--- services/backend/src/hello-world.js
+++ services/backend/src/hello-world.js
@@ -2,9 +2,6 @@ exports.handler = async (event) => {
     console.info(`Received: ${event}`);
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            event,
-            message: 'Hello world!',
-        }),
+        body: 'Hello world!',
     };
 };
