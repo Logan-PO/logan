57744044dd10ecdb8e461c081ea173488f473c53
Lucas Popp <lmp122@case.edu>
2020-09-15 19:32:15 -0400

    Update BE deployment script to wrk with Lerna

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -1,4 +1,5 @@
 npm run clean-all
 lerna bootstrap --scope @logan/backend --include-dependencies -- --production --no-optional
+node services/backend/scripts/copy-dependencies.js
 cd services/backend
 eb deploy
