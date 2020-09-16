210424e7d6b7d469eec860acfc42eaaca0edf9e4
Adam Stewart <adamjoesph11@gmail.com>
2020-09-16 18:12:40 -0400

    updating index to be able to access assignments page

--- services/frontend/src/pages/index.js
+++ services/frontend/src/pages/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react"
+import "../styles/global.css"
+import { Link } from "gatsby"
+import Container from "../components/containter"
+
 
 export default function Home() {
-    return <div>Hello world!</div>;
+  return <Container>
+    <h1>Overview Page (WIP)</h1><Link to="/assignments/">Assignments</Link>
+  </Container>
 }
