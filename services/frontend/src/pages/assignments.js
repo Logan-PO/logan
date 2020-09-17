5a156b0d55ac069cd81bee58faf0acc29ea5e8a6
Adam Stewart <adamjoesph11@gmail.com>
2020-09-16 20:35:49 -0400

    adding possible base implementation for assignments view

--- services/frontend/src/pages/assignments.js
+++ services/frontend/src/pages/assignments.js
@@ -1,12 +1,18 @@
 import React from "react"
 
 import Container from "../components/containter"
+import assignmentDay from "../components/assignment-day"
+
 import { Link } from "gatsby"
 
 export default function About() {
+    const ass = <assignment name="Test" desc={"test Test"}>ok</assignment>;
+    const assignments1 = <assignmentIn day="Tues" assignments ={ass}>ok</assignmentIn>
     return ( <Container>
             <h1>Assignments</h1>
-    <p>Assignments go here</p>
+    <p>
+        {assignmentDay(assignments1)}
+    </p>
             <div><Link to="../">Back to Overview</Link></div>
     </Container>
     )
