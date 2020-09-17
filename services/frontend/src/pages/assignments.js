6ba18d48761ecda371e99ab60f92db4863add91b
Adam Stewart <adamjoesph11@gmail.com>
2020-09-16 22:27:00 -0400

    adding dummy implementation of assignment interface

--- services/frontend/src/pages/assignments.js
+++ services/frontend/src/pages/assignments.js
@@ -1,19 +1,34 @@
 import React from "react"
-
+import classColors from "../styles/assignments.css"
+import { Link } from "gatsby"
 import Container from "../components/containter"
 import assignmentDay from "../components/assignment-day"
+//<div className={isBackgroundRed ? 'background-red' : 'background-blue'} />
 
-import { Link } from "gatsby"
+let assignmentsList = [
+    {id: 35, name: 'Lab', color: 'red', class: 'PHSY 101', desc: "sad"},
+    {id: 42, name: 'Project', color: 'blue', class: 'EECS 293', desc: "happy"},
+    {id: 71, name: 'Grading', color: 'green', class: 'all', desc: "?"},
+]
 
-export default function About() {
-    const ass = <assignment name="Test" desc={"test Test"}>ok</assignment>;
-    const assignments1 = <assignmentIn day="Tues" assignments ={ass}>ok</assignmentIn>
-    return ( <Container>
+
+
+
+export default function DisplayAssignments() {
+
+    let items = assignmentsList.map((item) =>
+        <li key={item.id}>
+            <h3 style={{backgroundColor:item.color}}>{item.class}</h3>
+            <div>Assignment: {item.name} <div>Desc: {item.desc}</div> </div>
+        </li>
+    );
+
+    return ( <div><Container>
             <h1>Assignments</h1>
-    <p>
-        {assignmentDay(assignments1)}
-    </p>
+
+        <ul>{items}</ul>
+
             <div><Link to="../">Back to Overview</Link></div>
-    </Container>
+    </Container> <button style={{backgroundColor:'grey'}} onClick={AddAssignment()}>Add Assignment</button> </div>
     )
 }
