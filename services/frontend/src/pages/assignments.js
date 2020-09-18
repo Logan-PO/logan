55a8b9e836193625e050074de4a1d178a2b04978
Adam Stewart <adamjoesph11@gmail.com>
2020-09-18 13:14:12 -0400

    adding redux to dependencies and reworking assignments.js to use Redux

--- services/frontend/src/pages/assignments.js
+++ services/frontend/src/pages/assignments.js
@@ -2,25 +2,47 @@ import React from "react"
 import classColors from "../styles/assignments.css"
 import { Link } from "gatsby"
 import Container from "../components/containter"
+import { configureStore } from '@reduxjs/toolkit'
+import items from "../components/assignmentDaySlice"
+
+/**
+ * The React component that shows the UI for the assignments feature
+ * @type {({color: string, name: string, id: number, class: string, desc: string}|{color: string, name: string, id: number, class: string, desc: string}|{color: string, name: string, id: number, class: string, desc: string})[]}
+ */
 
 
-let assignmentsList = [
-    {id: 35, name: 'Lab', color: 'red', class: 'PHSY 101', desc: "sad"},
-    {id: 42, name: 'Project', color: 'blue', class: 'EECS 293', desc: "happy"},
-    {id: 71, name: 'Grading', color: 'green', class: 'all', desc: "?"},
-]
+const allAssignments = {
+    type: 'assignments/fetchAssignments',
+    payload: 'assignments'
+}//state -> view -> actions -> state ...
 
+const addAssignment = text => {
+    return {
+        type: 'assignments/addAssignment',
+        payload: text
+    }
 
+}
 
+const initialState = {value: 0}
+
+function assignmentReducer(state = initialState, action){
+    if (action.type === 'assignments/addAssignment'){
+        return {
+            ...state,//return a copy of the state plus
+            value: state.value + 1//the new value its storing
+        }
+    }
+    return state
+}
+
+const store = configureStore({reducer : assignmentReducer})
+
+console.log(store.getState())
 
 export default function DisplayAssignments() {
 
-    let items = assignmentsList.map((item) =>
-        <li key={item.id}>
-            <h3 style={{backgroundColor:item.color}}>{item.class}</h3>
-            <div>Assignment: {item.name} <div>Desc: {item.desc}</div> </div>
-        </li>
-    );
+    //get all assignment days and parse them
 
     return ( <div><Container>
             <h1>Assignments</h1>
@@ -28,6 +50,7 @@ export default function DisplayAssignments() {
         <ul>{items}</ul>
 
             <div><Link to="../">Back to Overview</Link></div>
-    </Container> <button style={{backgroundColor:'grey'}} onClick={AddAssignment()}>Add Assignment</button> </div>
-    )
+    </Container> <button style={{backgroundColor:'grey'}} onClick={text => dispatch(addAssignment(text))} >Add Assignment</button> </div>
+    )//The color will be stored in the course so the color would be pulled from course ID
+    //Put assignment under its due date
 }
