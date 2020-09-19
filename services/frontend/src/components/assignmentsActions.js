27d00a007bef191e3f4b8ed2a1e17a65107de848
Adam Stewart <adamjoesph11@gmail.com>
2020-09-19 13:20:35 -0400

    adding actions for assignments page

---
+++ services/frontend/src/components/assignmentsActions.js
@@ -0,0 +1,17 @@
+export const addAssignment = () => {
+    return{
+        type: 'addAssignment'
+    };
+};
+
+export const deleteAssignment = () => {
+    return{
+        type: 'deleteAssignment'
+    };
+};
+
+export const editAssignment = () => {
+    return{
+        type: 'editAssignment'
+    };
+};
