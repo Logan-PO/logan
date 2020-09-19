5fea455e7fddd70e14ab8da1234c773012b953e5
Lucas Popp <lmp122@case.edu>
2020-09-19 01:30:43 -0400

    Add getTask

--- services/backend/src/task-controller.js
+++ services/backend/src/task-controller.js
@@ -1,4 +1,32 @@
-async function getTask(req, res) {}
+const _ = require('lodash');
+const { AWS } = require('@logan/aws');
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+function fromDbFormat(db) {
+    return {
+        ..._.pick(db, ['uid', 'tid', 'title', 'aid', 'cid']),
+        description: db.desc,
+        dueDate: db.due,
+        priority: db.pri,
+        complete: db.cmp,
+    };
+}
+
+async function getTask(req, res) {
+    const dbResponse = await dynamo
+        .get({
+            TableName: 'tasks',
+            Key: { tid: req.params.tid },
+        })
+        .promise();
+
+    if (dbResponse.Item) {
+        res.json(fromDbFormat(dbResponse.Item));
+    } else {
+        throw new Error('Task does not exist');
+    }
+}
 
 async function getTasks(req, res) {}
 
