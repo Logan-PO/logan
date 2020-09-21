e381c2122bdcc420d7f8204fc91495b2575f7aba
Lucas Popp <lmp122@case.edu>
2020-09-21 13:16:58 -0400

    Move be dependencies script back

--- scripts/copy-be-dependencies.js
+++ scripts/copy-be-dependencies.js
@@ -2,15 +2,13 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 const cwd = execSync('pwd').toString('utf8').trim();
-
 if (path.basename(cwd) !== 'logan') throw new Error('Please run this script from the repo root');
 
-const nodeScriptOutput = execSync('cd services/backend && npm run list-dependencies').toString();
-const dependenciesString = nodeScriptOutput.match(/\[.+/ms);
-
-if (!dependenciesString[0]) throw new Error('Could not find dependencies');
-
-const dependencies = JSON.parse(dependenciesString[0]);
+const dependencies = JSON.parse(
+    execSync(
+        'node_modules/.bin/lerna la --scope @logan/backend --include-dependencies --json --loglevel silent'
+    ).toString()
+);
 
 for (const dependency of dependencies) {
     if (dependency.name === '@logan/backend') continue;
