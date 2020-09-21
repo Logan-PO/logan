cc90792080932e5631d672d0af13bf0a3e7d12eb
Lucas Popp <lmp122@case.edu>
2020-09-21 17:40:02 -0400

    Fix deploy action (#15)
    * Fix lerna installation and add working directories for deploy-fe
    
    * Add trigger to locally test
    
    * Revert global Lerna install
    
    * Add bootstrap:backend
    
    * Fix pr trigger
    
    * Move be dependencies script to root
    
    * Configure the AWS CLI for the frontend
    
    * Install lerna globally for backend deployment
    
    * Move copy-dependencies back
    
    * Use node script for copying dependencies
    
    * Remove global Lerna install
    
    * Move be dependencies script back
    
    * Try to set working directory in deploy step
    
    * Try to fix working directory
    
    * Add cd to deploy command
    
    * Use manual EB CLI installation instead of third party action
    
    * Try using pip3
    
    * Try using an artifact to deploy
    
    * Remove extra action trigger
    
    * Use npm script for bundling

--- scripts/copy-be-dependencies.js
+++ scripts/copy-be-dependencies.js
@@ -1,9 +1,14 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-const dependencies = JSON.parse(execSync('lerna la --scope @logan/backend --include-dependencies --json').toString());
-
 const cwd = execSync('pwd').toString('utf8').trim();
+if (path.basename(cwd) !== 'logan') throw new Error('Please run this script from the repo root');
+
+const dependencies = JSON.parse(
+    execSync(
+        'node_modules/.bin/lerna la --scope @logan/backend --include-dependencies --json --loglevel silent'
+    ).toString()
+);
 
 for (const dependency of dependencies) {
     if (dependency.name === '@logan/backend') continue;
