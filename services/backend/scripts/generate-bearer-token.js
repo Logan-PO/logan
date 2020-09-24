db7e6a22d96dcc67dc051afffdb0ce52a30f92c2
Lucas Popp <lmp122@case.edu>
2020-09-24 13:24:54 -0400

    Add instructions

--- services/backend/scripts/generate-bearer-token.js
+++ services/backend/scripts/generate-bearer-token.js
@@ -7,6 +7,13 @@ const { generateBearerToken } = require('../utils/auth');
 async function run() {
     const { username, clientType = 'web' } = argv;
 
+    if (!username) {
+        console.log('  No username provided.');
+        console.log('  Valid usages: node generate-bearer-token.js --username <uname>');
+        console.log('                node generate-bearer-token.js --username <uname> --client-type web');
+        return;
+    }
+
     console.log(`Searching for user: ${username}`);
     const response = await dynamoUtils.scan({
         TableName: 'users',
@@ -20,7 +27,7 @@ async function run() {
         const bearer = await generateBearerToken({ uid: user.uid }, clientType);
         execSync(`echo ${bearer} | pbcopy`);
         console.log('Bearer token copied to clipboard');
-        console.log();
+        console.log('\nToken:');
         console.log(bearer);
     } else {
         throw new Error('User not found');
