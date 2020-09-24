d9b5decd6ee421c77b8c1906b23cf4322226dd13
Lucas Popp <lmp122@case.edu>
2020-09-24 11:46:36 -0400

    Print bearer at end of script

--- services/backend/scripts/generate-bearer-token.js
+++ services/backend/scripts/generate-bearer-token.js
@@ -20,6 +20,8 @@ async function run() {
         const bearer = await generateBearerToken({ uid: user.uid }, clientType);
         execSync(`echo ${bearer} | pbcopy`);
         console.log('Bearer token copied to clipboard');
+        console.log();
+        console.log(bearer);
     } else {
         throw new Error('User not found');
     }
