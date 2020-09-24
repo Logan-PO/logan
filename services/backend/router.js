60075b3a5ea660b83f6cb06b092167b0a2529dc8
Lucas Popp <lmp122@case.edu>
2020-09-24 12:25:04 -0400

    Separate routes

--- services/backend/router.js
+++ services/backend/router.js
@@ -6,27 +6,9 @@ const tasksController = require('./src/tasks-controller');
 const assignmentsController = require('./src/assignments-controller');
 const termsController = require('./src/terms-controller');
 
-// A map of routes/HTTP methods to handlers
-// Add authRequired: true to a route to indicate that the user must be logged in
-const handlers = {
+const unauthedRoutes = {
     '/auth/verify': {
-        post: {
-            handler: require('./src/verify-id-token').verifyIdToken,
-        },
-    },
-    '/users/:uid': {
-        get: {
-            authRequired: true,
-            handler: usersController.getUser,
-        },
-        put: {
-            authRequired: true,
-            handler: usersController.updateUser,
-        },
-        delete: {
-            authRequired: true,
-            handler: usersController.deleteUser,
-        },
+        post: require('./src/verify-id-token').verifyIdToken,
     },
     '/users': {
         post: {
@@ -34,77 +16,40 @@ const handlers = {
             handler: usersController.createUser,
         },
     },
+};
+
+const authedRoutes = {
+    '/users/:uid': {
+        get: usersController.getUser,
+        put: usersController.updateUser,
+        delete: usersController.deleteUser,
+    },
     '/assignments/:aid': {
-        get: {
-            authRequired: true,
-            handler: assignmentsController.getAssignment,
-        },
-        put: {
-            authRequired: true,
-            handler: assignmentsController.updateAssignment,
-        },
-        delete: {
-            authRequired: true,
-            handler: assignmentsController.deleteAssignment,
-        },
+        get: assignmentsController.getAssignment,
+        put: assignmentsController.updateAssignment,
+        delete: assignmentsController.deleteAssignment,
     },
     '/assignments': {
-        get: {
-            authRequired: true,
-            handler: assignmentsController.getAssignments,
-        },
-        post: {
-            authRequired: true,
-            handler: assignmentsController.createAssignment,
-        },
+        get: assignmentsController.getAssignments,
+        post: assignmentsController.createAssignment,
     },
     '/tasks/:tid': {
-        get: {
-            authRequired: true,
-            handler: tasksController.getTask,
-        },
-        put: {
-            authRequired: true,
-            handler: tasksController.updateTask,
-        },
-        delete: {
-            authRequired: true,
-            handler: tasksController.deleteTask,
-        },
+        get: tasksController.getTask,
+        put: tasksController.updateTask,
+        delete: tasksController.deleteTask,
     },
     '/tasks': {
-        get: {
-            authRequired: true,
-            handler: tasksController.getTasks,
-        },
-        post: {
-            authRequired: true,
-            handler: tasksController.createTask,
-        },
+        get: tasksController.getTasks,
+        post: tasksController.createTask,
     },
     '/terms/:tid': {
-        get: {
-            authRequired: true,
-            handler: termsController.getTerm,
-        },
-        put: {
-            authRequired: true,
-            handler: termsController.updateTerm,
-        },
-        delete: {
-            authRequired: true,
-            handler: termsController.deleteTerm,
-        },
+        get: termsController.getTerm,
+        put: termsController.updateTerm,
+        delete: termsController.deleteTerm,
     },
     '/terms': {
-        get: {
-            authRequired: true,
-            handler: termsController.getTerms,
-        },
-        post: {
-            authRequired: true,
-            handler: termsController.createTerm,
-        },
+        get: termsController.getTerms,
+        post: termsController.createTerm,
     },
 };
 
@@ -115,6 +60,22 @@ const handlers = {
 function route(app) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
+
+    const handlers = _.merge(
+        {},
+        _.mapValues(unauthedRoutes, (methodMap) =>
+            _.mapValues(methodMap, (value) => {
+                if (typeof value === 'function') return { handler: value };
+                return value;
+            })
+        ),
+        _.mapValues(authedRoutes, (methodMap) =>
+            _.mapValues(methodMap, (value) => {
+                if (typeof value === 'function') return { authRequired: true, handler: value };
+                return { authRequired: true, ...value };
+            })
+        )
+    );
 
     for (const path of _.keys(handlers)) {
         for (const method of _.keys(handlers[path])) {
