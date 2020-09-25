a94e9f777916682aac20457e53d2978d01bb4e86
prao3 <premjrao@gmail.com>
2020-09-25 12:39:10 -0400

    Adding redux to the dependencies (cause I forgot to last time)
    Exporting a store and some actions from GoogleStore.js

--- services/frontend/src/components/GoogleStore.js
+++ services/frontend/src/components/GoogleStore.js
@@ -1,17 +1,25 @@
-export const login = (state = { isLoggedIn: false }, action) => {
+import { createStore } from 'redux';
+
+//A login action
+export const login = () => ({ type: 'login' });
+//A logout action
+export const logout = () => ({ type: 'logout' });
+
+/*
+ * A reducer for logging in and out
+ * Takes in a state (as all reducers should); Default logged out
+ * The action should have a type field specifying if we want to log in our out
+ */
+export const loginReducer = (state = { isLoggedIn: false }, action) => {
     switch (action.type) {
         case 'login':
             return { isLoggedIn: true };
-        default:
-            return state;
-    }
-};
-
-export const logout = (state = { isLoggedIn: false }, action) => {
-    switch (action.type) {
         case 'logout':
             return { isLoggedIn: false };
         default:
             return state;
     }
 };
+
+//Creating the store
+export const googleStore = createStore(loginReducer);
