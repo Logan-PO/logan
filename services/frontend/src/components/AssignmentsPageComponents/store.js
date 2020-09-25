9b2614ac0088ee2d95e4f728f20bab941d8069b9
Adam Stewart <adamjoesph11@gmail.com>
2020-09-25 12:28:14 -0400

    Actions Reducer and Store created

--- services/frontend/src/components/AssignmentsPageComponents/store.js
+++ services/frontend/src/components/AssignmentsPageComponents/store.js
@@ -1,23 +1,82 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import AssignmentCatalog from "./AssignmentCatalog";
 
 const composeEnhancers =
     typeof window === 'object' &&
     (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         : compose);
+/**
+ * ACTIONS
+ */
+import NameForm from './addAssignmentForm'
+import Assignment from "./Assignment";
 
-export const store = createStore()
+/**
+ * This file is used to define all of the possible actions able to be taken within the assignments page
+ */
 
-/*
-export function configureStore() {
-    const middlewares = [];
-    const store = createStore(
-        null,
-        {},
-        composeEnhancers(applyMiddleware(...middlewares))
-    );
-    return store;
+/**
+ * this is used to define an action that the user can take to add an assignment
+ * @returns returns the attributes of the assignment that is to be created {{color: string, name: string, id: number, type: string, class: string, desc: string}}
+ */
+export const addAssignment = assignment => {
+    return{//TODO: the values in this return are temporary and should be filled in via user input through the addAssignmentForm.js
+        //The assignment form values will probably be collected via an export from the assignments page
+        type: 'addAssignment',
+       assignment: assignment
+    };
+};
+
+//TODO: This will also take in user input and use the id of the assignment to find and remove it from a given assignment day
+export const deleteAssignment = assignment => {
+    return{
+        type: 'deleteAssignment',
+        assignment: assignment
+    };
+};
+
+//TODO: This will look very similar to add assignment except it will use the existing ID and update the other fields according to user input
+export const editAssignment = (assignment,args) => {
+    return{
+        type: 'editAssignment',
+        args: args,
+        assignment: assignment
+    };
+};
+
+/**
+ * REDUXERS
+ * @param state
+ * @param action
+ * @returns {{assignmentCatalog: AssignmentCatalog}}
+ */
+const reducer = (state = {assignmentCatalog:new AssignmentCatalog({list: []}) },action) => {
+    const tempAssignmentCatalog = state.assignmentCatalog
+    switch (action.type){
+        case 'addAssignment':
+            const newAssignment = new Assignment({
+                id: action.id,
+                name: action.name,
+                class: action.class,
+                desc: action.desc,
+                day: action.day,
+                color: action.color})
+            tempAssignmentCatalog.addAssignment(newAssignment)
+            return {assignmentCatalog: tempAssignmentCatalog}
+        case 'deleteAssignment':
+            tempAssignmentCatalog.deleteAssignment(action.assignment)
+            return {assignmentCatalog: tempAssignmentCatalog}
+        case 'editAssignment' :
+            action.assignment.editAssignment(action.args)
+            return {assignmentCatalog: tempAssignmentCatalog}
+        default:
+            return  state
+    }
 }
 
-export default configureStore;
-*/
+export const store = createStore(combineReducers({ AssignmentCatalog : reducer}))
+
+
+
+
