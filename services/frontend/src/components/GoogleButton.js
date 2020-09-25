e1690649fb71bfad1a896073e4dcc956142b523d
prao3 <premjrao@gmail.com>
2020-09-25 13:40:32 -0400

    Moved the google button in GoogleButton.js back to a class implementation (this also fixed all of the ESLint errors)
    Also added the prop-types package for reasons unbeknownst to me

--- services/frontend/src/components/GoogleButton.js
+++ services/frontend/src/components/GoogleButton.js
@@ -1,39 +1,49 @@
 import React from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { connect } from 'react-redux';
+import * as PropTypes from 'prop-types';
 import { login, logout } from './GoogleStore';
 
 const clientID = '850674143860-haau84mtom7b06uqqhg4ei1jironoah3.apps.googleusercontent.com';
 
-function GoogleBtn({ isLoggedIn, login, logout }) {
-    return (
-        <div>
-            {isLoggedIn ? (
-                //Logout button and fields
-                <GoogleLogout
-                    clientId={clientID}
-                    buttonText="Logout"
-                    onLogoutSuccess={logout}
-                    onFailure={handleLogoutFailure}
-                >
-                    {}
-                </GoogleLogout>
-            ) : (
-                //Login button and fields
-                <GoogleLogin
-                    clientId={clientID}
-                    buttonText="Login"
-                    onSuccess={login}
-                    onFailure={handleLoginFailure}
-                    cookiePolicy={'single_host_origin'}
-                    responseType="code,token"
-                >
-                    {}
-                </GoogleLogin>
-            )}
-        </div>
-    );
+class GoogleBtn extends React.Component {
+    render() {
+        let { isLoggedIn, login, logout } = this.props;
+        return (
+            <div>
+                {isLoggedIn ? (
+                    //Logout button and fields
+                    <GoogleLogout
+                        clientId={clientID}
+                        buttonText="Logout"
+                        onLogoutSuccess={logout}
+                        onFailure={handleLogoutFailure}
+                    >
+                        {}
+                    </GoogleLogout>
+                ) : (
+                    //Login button and fields
+                    <GoogleLogin
+                        clientId={clientID}
+                        buttonText="Login"
+                        onSuccess={login}
+                        onFailure={handleLoginFailure}
+                        cookiePolicy={'single_host_origin'}
+                        responseType="code,token"
+                    >
+                        {}
+                    </GoogleLogin>
+                )}
+            </div>
+        );
+    }
 }
+
+GoogleBtn.propTypes = {
+    isLoggedIn: PropTypes.any,
+    login: PropTypes.any,
+    logout: PropTypes.any,
+};
 
 //If we fail to log in, print the response
 function handleLoginFailure(response) {
