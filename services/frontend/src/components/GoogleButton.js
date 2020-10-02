09ac4b77f19a779349df5a4a3fa489e88d2e6ef5
prao3 <premjrao@gmail.com>
2020-10-02 12:00:02 -0400

    Fixing asynchronous action issue
    Can now post the id token to the backend and call the login action asynchronously, hopefully without any issues

--- services/frontend/src/components/GoogleButton.js
+++ services/frontend/src/components/GoogleButton.js
@@ -2,13 +2,37 @@ import React from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { connect } from 'react-redux';
 import * as PropTypes from 'prop-types';
+import axios from 'axios';
 import { login, logout } from './GoogleStore';
 
 const clientID = '850674143860-haau84mtom7b06uqqhg4ei1jironoah3.apps.googleusercontent.com';
+const authURI = 'http://logan-backend-dev.us-west-2.elasticbeanstalk.com/auth/verify';
 
 class GoogleBtn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onLogin = this.onLogin.bind(this);
+    }
+
+    async onLogin(response) {
+        let login = this.props.login;
+        axios
+            .post(authURI, { idToken: response.tokenId })
+            .then((res) => {
+                console.log(res);
+                console.log('successful');
+                login();
+            })
+            .catch((error) => {
+                console.log(error);
+                console.log('errored out');
+            });
+    }
+
     render() {
-        let { isLoggedIn, login, logout } = this.props;
+        let isLoggedIn = this.props.isLoggedIn;
+        let logout = this.props.logout;
         return (
             <div>
                 {isLoggedIn ? (
@@ -26,7 +50,7 @@ class GoogleBtn extends React.Component {
                     <GoogleLogin
                         clientId={clientID}
                         buttonText="Login"
-                        onSuccess={login}
+                        onSuccess={this.onLogin}
                         onFailure={handleLoginFailure}
                         cookiePolicy={'single_host_origin'}
                         //responseType="code,token"
--- services/frontend/src/components/GoogleStore.js
+++ services/frontend/src/components/GoogleStore.js
@@ -1,27 +1,13 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
-import axios from 'axios';
-
-const authURI = 'http://logan-backend-dev.us-west-2.elasticbeanstalk.com/auth/verify';
 
 /*
  * A login action
  * Takes in a response, which should be from the google button
  * The response should contain relevant keys and stuff for OAuth
  */
-export function login(response) {
-    //console.log(response.hasOwnProperty('tokenId'));
-    let newType = 'do nothing';
-    axios
-        .post(authURI, { idToken: response.tokenId })
-        .then((res) => {
-            newType = 'login';
-            console.log(res);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    return { type: newType };
+export function login() {
+    return { type: 'login' };
 }
 
 /*
