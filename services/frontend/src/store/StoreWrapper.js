e60222087d0bc0f169b97bb488f549ea0000d299
Lucas Popp <lmp122@case.edu>
2020-10-04 12:16:13 -0400

    Export store instead of createStore

--- services/frontend/src/store/StoreWrapper.js
+++ services/frontend/src/store/StoreWrapper.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import createStore from '.';
+import store from '.';
 
 function wrapper({ element }) {
-    const store = createStore();
     return <Provider store={store}>{element}</Provider>;
 }
 
--- services/frontend/src/store/index.js
+++ services/frontend/src/store/index.js
@@ -2,5 +2,6 @@ import { createStore, combineReducers } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({});
+const store = createStore(rootReducer, devToolsEnhancer());
 
-export default () => createStore(rootReducer, devToolsEnhancer());
+export default store;
