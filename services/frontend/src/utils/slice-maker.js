f90d79fa03182c1fee585388a5468627997218ea
Lucas Popp <lmp122@case.edu>
2020-10-04 17:50:27 -0400

    Add more handlers to slice-maker

--- services/frontend/src/utils/slice-maker.js
+++ services/frontend/src/utils/slice-maker.js
@@ -2,22 +2,30 @@ import _ from 'lodash';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 /**
- *
- * @param config
- * @param config.name - The slice name
+ * The configuration for an async reducer
+ * @typedef {Object} AsyncReducerConfig
+ * @property {function} fn - The body of the async thunk
+ * @property {function} [begin] - Called when execution begins
+ * @property {function} [success] - Called if the promise resolves
+ * @property {function} [failure] - Called if the promise rejects
+ */
+
+/**
+ * @param {Object} config
+ * @param {string} config.name
  * @param config.initialState
- * @param [config.reducers]
- * @param [config.extraReducers]
- * @param [config.asyncReducers]
+ * @param {Object} [config.reducers]
+ * @param {Object} [config.extraReducers]
+ * @param {Object.<string, AsyncReducerConfig>} [config.asyncReducers]
  */
 export default function createAsyncSlice(config) {
     // Create any thunks necesssary
     const asyncReducers = {};
     if (config.asyncReducers) {
-        for (const [name, { fn, handler }] of _.entries(config.asyncReducers)) {
+        for (const [name, { fn, ...handlers }] of _.entries(config.asyncReducers)) {
             asyncReducers[name] = {
                 thunk: createAsyncThunk(`${config.name}/${name}`, fn),
-                handler,
+                handlers,
             };
         }
     }
@@ -30,8 +38,10 @@ export default function createAsyncSlice(config) {
     };
 
     // Set up their handlers as extraReducers
-    for (const { thunk, handler } of _.values(asyncReducers)) {
-        sliceParams.extraReducers[thunk.fulfilled] = handler;
+    for (const { thunk, handlers } of _.values(asyncReducers)) {
+        if (handlers.begin) sliceParams.extraReducers[thunk.pending] = handlers.begin;
+        if (handlers.success) sliceParams.extraReducers[thunk.fulfilled] = handlers.success;
+        if (handlers.failure) sliceParams.extraReducers[thunk.rejected] = handlers.failure;
     }
 
     return {
