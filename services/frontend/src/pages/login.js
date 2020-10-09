e2f5e0b154e9c78b53a4a60072680d0df1bcaa67
prao3 <44528013+prao3@users.noreply.github.com>
2020-10-09 14:46:36 -0400

    Adding some new text to the login page

--- services/frontend/src/pages/login.js
+++ services/frontend/src/pages/login.js
@@ -7,12 +7,16 @@ export default function Login() {
     return (
         <div>
             <div>
-                <h1>Login</h1>
+                <h1>Welcome to Logan!</h1>
             </div>
             <Provider store={googleStore}>
                 {' '}
                 <GoogleBtn />{' '}
             </Provider>
+            <div>
+                <h1>About Logan</h1>
+                <p>Stuff about Logan goes here.</p>
+            </div>
         </div>
     );
 }
