0de6db6084bea1d1869d578ac14c4afa12569bb5
Lucas Popp <lmp122@case.edu>
2020-10-12 13:35:53 -0400

    Speed up clearTables

--- services/backend/utils/test-utils.js
+++ services/backend/utils/test-utils.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { dynamoUtils } = require('@logan/aws');
 const Promise = require('bluebird');
 
@@ -12,16 +13,23 @@ const tableKeyMap = {
 };
 
 async function clearTables(tables = undefined) {
-    if (!tables) tables = Object.values(dynamoUtils.TABLES);
-    await Promise.map(tables, tableName => clearTable(tableName));
+    if (!tables) tables = _.values(dynamoUtils.TABLES);
+    const itemsByTable = {};
+
+    await Promise.map(tables, async table => {
+        itemsByTable[table] = (await dynamoUtils.scan({ TableName: table, AutoPaginate: true })).Items;
+    });
+
+    const requests = _.mapValues(itemsByTable, (items, table) => {
+        const pk = tableKeyMap[table];
+        return items.map(item => ({ DeleteRequest: { Key: { [pk]: item[pk] } } }));
+    });
+
+    await dynamoUtils.batchWrite(requests);
 }
 
 async function clearTable(table) {
-    const pk = tableKeyMap[table];
-    const { Items: toDelete } = await dynamoUtils.scan({ TableName: table, AutoPaginate: true });
-    if (!toDelete.length) return;
-    const deleteRequests = toDelete.map(item => ({ DeleteRequest: { Key: { [pk]: item[pk] } } }));
-    await dynamoUtils.batchWrite({ [table]: deleteRequests });
+    return clearTables([table]);
 }
 
 module.exports = {
