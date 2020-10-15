ed155244bedb7767af4952912d1e124f3f31045e
Lucas Popp <lmp122@case.edu>
2020-10-15 00:39:27 -0400

    Add more controls to task editor

--- services/frontend/src/components/tasks/task-editor.js
+++ services/frontend/src/components/tasks/task-editor.js
@@ -2,15 +2,25 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Grid, TextField } from '@material-ui/core';
+import { Grid, TextField, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio } from '@material-ui/core';
+import dayjs from 'dayjs';
 import UpdateTimer from '../../utils/update-timer';
 import { getTasksSelectors, updateTaskLocal, updateTask, deleteTask } from '../../store/tasks';
 import styles from './task-editor.module.scss';
+
+const priorities = {
+    'Very low': -2,
+    Low: -1,
+    Normal: 0,
+    High: 1,
+    'Very high': 2,
+};
 
 class TaskEditor extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.updateDueDateType = this.updateDueDateType.bind(this);
 
         this.changesExist = false;
         this.updateTimer = new UpdateTimer(1000, () => {
@@ -18,9 +28,19 @@ class TaskEditor extends React.Component {
             this.changesExist = false;
         });
 
-        this.state = {
-            task: undefined,
-        };
+        this.state = {};
+    }
+
+    updateCurrentTask(task) {
+        let dueDateType = _.get(task, 'dueDate');
+        if (dueDateType !== 'asap' && dueDateType !== 'eventually') {
+            dueDateType = 'date';
+        }
+
+        this.setState({
+            task,
+            dueDateType,
+        });
     }
 
     componentDidUpdate(prevProps) {
@@ -32,17 +52,34 @@ class TaskEditor extends React.Component {
                 if (prevTask) this.updateTimer.fire();
 
                 this.updateTimer.stop();
+                this.setState({ lastDueDate: dayjs() });
             }
 
-            this.setState({
-                task: this.props.selectTask(this.props.tid),
-            });
+            this.updateCurrentTask(this.props.selectTask(this.props.tid));
         } else {
             // Also if the task has been updated somewhere else, make sure the state reflects that
             const storeTask = this.props.selectTask(this.props.tid);
             if (!_.isEqual(storeTask, this.state.task)) {
-                this.setState({ task: storeTask });
+                this.updateCurrentTask(storeTask);
             }
+        }
+    }
+
+    updateDueDateType(e) {
+        const newType = e.target.value;
+
+        this.changesExist = true;
+
+        if (newType === 'date') {
+            let lastDueDate = this.state.lastDueDate;
+            if (!this.state.lastDueDate) {
+                lastDueDate = dayjs();
+                this.setState({ lastDueDate });
+            }
+
+            this.makeChanges({ dueDate: lastDueDate.format('M/D/YYYY') });
+        } else {
+            this.makeChanges({ dueDate: newType });
         }
     }
 
@@ -51,16 +88,25 @@ class TaskEditor extends React.Component {
 
         const changes = {};
 
-        changes[prop] = e.target.value;
+        if (prop === 'dueDate') {
+            const dateString = e.target.value;
+            const dateObj = dayjs(dateString, 'YYYY-MM-DD');
+            this.setState({ lastDueDate: dateObj });
+            changes[prop] = dateObj.format('M/D/YYYY');
+        } else {
+            changes[prop] = e.target.value;
+        }
 
+        this.makeChanges(changes);
+    }
+
+    makeChanges(changes) {
         this.props.updateTaskLocal({
             id: this.props.tid,
             changes,
         });
 
-        this.setState({
-            task: _.merge({}, this.state.task, changes),
-        });
+        this.updateCurrentTask(_.merge({}, this.state.task, changes));
 
         this.updateTimer.reset();
     }
@@ -75,6 +121,7 @@ class TaskEditor extends React.Component {
                             fullWidth
                             onChange={this.handleChange.bind(this, 'title')}
                             value={_.get(this.state.task, 'title', '')}
+                            color="secondary"
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -84,7 +131,63 @@ class TaskEditor extends React.Component {
                             multiline
                             onChange={this.handleChange.bind(this, 'description')}
                             value={_.get(this.state.task, 'description', '')}
+                            color="secondary"
                         />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                            <FormLabel color="secondary">Due Date</FormLabel>
+                            <RadioGroup
+                                name="dueDateType"
+                                value={_.get(this.state, 'dueDateType', '')}
+                                onChange={this.updateDueDateType}
+                            >
+                                <FormControlLabel
+                                    value="asap"
+                                    label="ASAP"
+                                    labelPlacement="end"
+                                    control={<Radio color="secondary" />}
+                                />
+                                <FormControlLabel
+                                    value="eventually"
+                                    label="Eventually"
+                                    labelPlacement="end"
+                                    control={<Radio color="secondary" />}
+                                />
+                                <FormControlLabel
+                                    value="date"
+                                    label={
+                                        <TextField
+                                            type="date"
+                                            disabled={_.get(this.state, 'dueDateType') !== 'date'}
+                                            value={_.get(this.state, 'lastDueDate', dayjs()).format('YYYY-MM-DD')}
+                                            onChange={this.handleChange.bind(this, 'dueDate')}
+                                            color="secondary"
+                                        />
+                                    }
+                                    labelPlacement="end"
+                                    control={<Radio color="secondary" />}
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                        <FormControl component="fieldset">
+                            <FormLabel color="secondary">Priority</FormLabel>
+                            <RadioGroup
+                                name="priority"
+                                value={_.get(this.state.task, 'priority', 0)}
+                                onChange={this.handleChange.bind(this, 'priority')}
+                            >
+                                {_.entries(priorities).map(([pName, p]) => (
+                                    <FormControlLabel
+                                        key={pName}
+                                        value={p}
+                                        label={pName}
+                                        labelPlacement="end"
+                                        control={<Radio color="secondary" />}
+                                    />
+                                ))}
+                            </RadioGroup>
+                        </FormControl>
                     </Grid>
                 </Grid>
             </div>
