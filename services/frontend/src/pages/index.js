2faafecd503e01da245d73fe211c5f443f73a21f
prao3 <44528013+prao3@users.noreply.github.com>
2020-10-18 11:21:09 -0500

    Fixing index so a link takes us to login/logout page

--- services/frontend/src/pages/index.js
+++ services/frontend/src/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby'
+import { Link } from 'gatsby';
 
 export default function Home() {
     return (
         <div>
             <div>Hello world!</div>
-            <Link to=".\login">Login/Logout</Link>
+            <Link to="login">Login/Logout</Link>
         </div>
     );
 }
