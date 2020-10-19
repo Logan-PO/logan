910aa7f9a9ccb0f6ca9c691fdc5f6aceea890fd8
jzassick <60803004+jzassick@users.noreply.github.com>
2020-10-19 10:20:54 -0400

    added basic 404

---
+++ services/frontend/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PageNotFound() {
+    return (
+        <div>
+            <h1>Page not found</h1>
+            <p>We cannot seem to find the page you are looking for.</p>
+        </div>
+    );
+}
