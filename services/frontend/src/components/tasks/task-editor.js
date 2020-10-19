bdca029c9647bf9447ab3ad2abbb658f8eaabe07
Adam Stewart <adamjoesph11@gmail.com>
2020-10-18 21:02:30 -0400

    fixing linting errors

--- services/frontend/src/components/tasks/task-editor.js
+++ services/frontend/src/components/tasks/task-editor.js
@@ -130,12 +130,7 @@ class TaskEditor extends React.Component {
 
         this.updateTimer.reset();
     }
-    /*class: 'PHYS',
-    name: 'Random Assignment',
-    desc: 'test',
-    dueDate: 'soon',
-    color: 'orange',
-    id: 1,*/
+
     render() {
         return (
             <div className={styles.taskEditor}>
