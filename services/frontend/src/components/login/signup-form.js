68714d193de589698d74c3e333a6b73fd4fbdbb3
Lucas Popp <lmp122@case.edu>
2020-10-19 13:53:37 -0400

    Upgrade signup-form to save state between pages and automatically redirect

--- services/frontend/src/components/login/signup-form.js
+++ services/frontend/src/components/login/signup-form.js
@@ -1,30 +1,36 @@
+import _ from 'lodash';
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { navigate } from 'gatsby';
-import { Grid, TextField, Button } from '@material-ui/core';
+import { Container, Grid, TextField, Button } from '@material-ui/core';
 import api from '../../utils/api';
+import { LOGIN_STAGE } from '../../store/login';
 import styles from './signup.modules.scss';
 
 class SignUpForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { name: '', username: '', email: '' };
 
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            name: _.get(props, ['meta', 'name'], ''),
+            username: '',
+            email: _.get(props, ['meta', 'email'], ''),
+        };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleNameChange(event) {
-        this.setState({ name: event.target.value });
+    componentDidMount() {
+        if (this.props.loginStage === LOGIN_STAGE.LOGIN) {
+            navigate('../login');
+        } else if (this.props.loginStage !== LOGIN_STAGE.CREATE) {
+            navigate('../');
+        }
     }
 
-    handleUsernameChange(event) {
-        this.setState({ username: event.target.value });
-    }
-
-    handleEmailChange(event) {
-        this.setState({ email: event.target.value });
+    handleChange(prop, e) {
+        this.setState({ [prop]: e.target.value });
     }
 
     async handleSubmit(event) {
@@ -37,25 +43,45 @@ class SignUpForm extends React.Component {
     render() {
         return (
             <div className={styles.signupForm}>
-                <Grid container spacing={2} direction="column">
-                    <Grid item xs={12}>
-                        <TextField label="Name" onChange={this.handleNameChange} value={this.state.name} />
+                <Container>
+                    <Grid container spacing={2} direction="column">
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Name"
+                                onChange={this.handleChange.bind(this, 'name')}
+                                value={this.state.name}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Username"
+                                onChange={this.handleChange.bind(this, 'username')}
+                                value={this.state.username}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Email" disabled value={this.state.email} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <TextField label="Username" onChange={this.handleUsernameChange} value={this.state.username} />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField label="Email" onChange={this.handleEmailChange} value={this.state.email} />
-                    </Grid>
-                </Grid>
-                <div>
-                    <Button variant="outlined" color="primary" onClick={this.handleSubmit}>
-                        Signup
-                    </Button>
-                </div>
+                    <div>
+                        <Button variant="outlined" color="primary" onClick={this.handleSubmit}>
+                            Signup
+                        </Button>
+                    </div>
+                </Container>
             </div>
         );
     }
 }
 
-export default SignUpForm;
+SignUpForm.propTypes = {
+    meta: PropTypes.object,
+    loginStage: PropTypes.string,
+};
+
+const mapStateToProps = state => ({
+    loginStage: state.login.currentStage,
+    meta: state.login.userMeta,
+});
+
+export default connect(mapStateToProps, null)(SignUpForm);
