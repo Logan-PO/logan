a2c246e07fd27778c77b3b3c0a882d698114d116
Lucas Popp <lmp122@case.edu>
2020-10-21 14:16:19 -0400

    Flip priorities order

--- services/frontend/src/components/tasks/priority-picker.js
+++ services/frontend/src/components/tasks/priority-picker.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import { FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, colors } from '@material-ui/core';
 
 const priorities = {
-    'Very low': [-2, colors.blue[500]],
-    Low: [-1, colors.green[500]],
-    Normal: [0, colors.yellow[500]],
-    High: [1, colors.orange[500]],
     'Very high': [2, colors.deepOrange[500]],
+    High: [1, colors.orange[500]],
+    Normal: [0, colors.yellow[500]],
+    Low: [-1, colors.green[500]],
+    'Very low': [-2, colors.blue[500]],
 };
 
 class PriorityPicker extends React.Component {
