414ba7051e92ee68f1a665f2c9b78b1885cd7de2
Lucas Popp <lmp122@case.edu>
2020-10-22 12:00:12 -0400

    Add course picker to frontend (#49)
    * Create a basic course picker
    
    * Remove lodash
    
    * Display no value if value does not exist
    
    * Add course picker to task editor
    
    * Add course picker to assignment editor
    
    * Add course labels to tasks and assignments
    
    * Add colored checkbox to task cell

---
+++ services/frontend/src/components/shared/controls/course-picker.js
@@ -0,0 +1,84 @@
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { FormControl, InputLabel, Select, ListSubheader, MenuItem } from '@material-ui/core';
+import { getScheduleSelectors } from '../../../store/schedule';
+
+class CoursePicker extends React.Component {
+    getPropsToForward() {
+        return _.omit(this.props, ['tids', 'getTerm', 'allCids', 'getCoursesForTerm', 'value', 'onChange']);
+    }
+
+    getDerivedValue() {
+        if (this.props.value !== 'none' && !this.props.allCids.includes(this.props.value)) return undefined;
+        else return this.props.value;
+    }
+
+    generateItems() {
+        const terms = this.props.tids.map(tid => {
+            const term = { ...this.props.getTerm(tid) };
+            term.courses = this.props.getCoursesForTerm(term);
+            return term;
+        });
+
+        const items = [];
+        items.push(
+            <MenuItem key="none" value="none">
+                None
+            </MenuItem>
+        );
+
+        for (const term of terms.filter(term => term.courses.length)) {
+            items.push(<ListSubheader key={term.tid}>{term.title}</ListSubheader>);
+            for (const course of term.courses) {
+                items.push(
+                    <MenuItem key={course.cid} value={course.cid} style={{ color: course.color }}>
+                        {course.nickname || course.title}
+                    </MenuItem>
+                );
+            }
+        }
+
+        return items;
+    }
+
+    render() {
+        return (
+            <FormControl disabled={this.props.disabled}>
+                <InputLabel>Course</InputLabel>
+                <Select value={this.getDerivedValue()} onChange={this.props.onChange} {...this.getPropsToForward()}>
+                    {this.generateItems()}
+                </Select>
+            </FormControl>
+        );
+    }
+}
+
+CoursePicker.propTypes = {
+    disabled: PropTypes.bool,
+    tids: PropTypes.array,
+    getTerm: PropTypes.func,
+    getCoursesForTerm: PropTypes.func,
+    allCids: PropTypes.array,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+};
+
+CoursePicker.defaultProps = {
+    tids: [],
+    value: 'none',
+};
+
+const mapStateToProps = state => {
+    const selectors = getScheduleSelectors(state.schedule);
+
+    return {
+        tids: selectors.baseSelectors.terms.selectIds(),
+        getTerm: selectors.baseSelectors.terms.selectById,
+        getCoursesForTerm: selectors.getCoursesForTerm,
+        allCids: selectors.baseSelectors.courses.selectIds(),
+    };
+};
+
+export default connect(mapStateToProps, null)(CoursePicker);
