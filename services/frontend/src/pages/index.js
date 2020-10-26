8f88ebcd95cf4603353c46b6acf0b38b3f0b6bd7
Lucas Popp <lmp122@case.edu>
2020-10-26 17:38:59 -0400

    Do login and all that via the landing page (#51)
    * Move login stuff to home page
    
    * Handle user creation on the home page
    
    * Add a link to the page

--- services/frontend/src/pages/index.js
+++ services/frontend/src/pages/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import HomePage from '../components/home/home-page';
 
-export default function Home() {
-    return (
-        <div>
-            <div>Hello world!</div>
-            <div>
-                <Link to="login">Login/Logout</Link>
-            </div>
-            <div>
-                <Link to="tasks">Tasks</Link>
-            </div>
-            <div>
-                <Link to="assignments">Assignments</Link>
-            </div>
-        </div>
-    );
+export default function wrapper() {
+    return <HomePage />;
 }
--- services/frontend/src/pages/login.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LoginPage from '../components/login/login-page';
-
-export default function wrapper() {
-    return <LoginPage />;
-}
