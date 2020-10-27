7ecc0e6e439dd3a573878c1d8bae52e38c62bafc
Lucas Popp <lmp122@case.edu>
2020-10-27 17:45:36 -0400

    Filter assignments by date

--- services/frontend/src/components/assignments/assignments-list.js
+++ services/frontend/src/components/assignments/assignments-list.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import AddIcon from '@material-ui/icons/Add';
-import { List, ListSubheader, Fab } from '@material-ui/core';
+import { List, ListSubheader, AppBar, Toolbar, FormControl, FormControlLabel, Switch, Fab } from '@material-ui/core';
+import _ from 'lodash';
+import { dateUtils } from '@logan/core';
 import {
     fetchAssignments,
     createAssignment,
@@ -12,17 +14,26 @@ import {
 } from '../../store/assignments';
 import AssignmentCell from './assignment-cell';
 import '../shared/list.scss';
+import classes from './assignments-list.module.scss';
 
 class AssignmentsList extends React.Component {
     constructor(props) {
         super(props);
 
+        this._shouldShowAssignment = this._shouldShowAssignment.bind(this);
+        this.sectionsToShow = this.sectionsToShow.bind(this);
         this.didSelectAssignment = this.didSelectAssignment.bind(this);
         this.didDeleteAssignment = this.didDeleteAssignment.bind(this);
+        this.togglePastAssignments = this.togglePastAssignments.bind(this);
 
         this.state = {
+            showingPastAssignments: false,
             selectedAssignment: undefined,
         };
+    }
+
+    togglePastAssignments(e) {
+        this.setState({ showingPastAssignments: e.target.checked });
     }
 
     randomAssignment() {
@@ -46,12 +57,48 @@ class AssignmentsList extends React.Component {
         this.props.onAssignmentSelected(undefined);
     }
 
+    _shouldShowAssignment(assignment) {
+        const today = dateUtils.dayjs();
+
+        if (this.state.showingPastAssignments) {
+            return dateUtils.dayjs(assignment.dueDate).isBefore(today, 'day');
+        } else {
+            return !dateUtils.dayjs(assignment.dueDate).isBefore(today, 'day');
+        }
+    }
+
+    sectionsToShow() {
+        const sections = {};
+        const assignmentsToUse = _.filter(this.props.assignments, this._shouldShowAssignment);
+
+        for (const assignment of assignmentsToUse) {
+            const key = dateUtils.dueDateIsDate(assignment.dueDate)
+                ? dateUtils.dayjs(assignment.dueDate)
+                : assignment.dueDate;
+            if (sections[key]) sections[key].push(assignment.aid);
+            else sections[key] = [assignment.aid];
+        }
+
+        // TODO: Better sorting
+        if (this.state.showingPastAssignments) {
+            return _.entries(sections)
+                .sort((a, b) => compareDueDates(b[0], a[0]))
+                .map(([key, value]) => [dateUtils.readableDueDate(key), value]);
+        } else {
+            return _.entries(sections)
+                .sort((a, b) => compareDueDates(a[0], b[0]))
+                .map(([key, value]) => [dateUtils.readableDueDate(key), value]);
+        }
+    }
+
     render() {
+        const sections = this.sectionsToShow();
+
         return (
             <div className="scrollable-list">
-                <div className="scroll-view">
+                <div className={`scroll-view ${classes.assignmentsList}`}>
                     <List>
-                        {this.props.sections.map(section => {
+                        {sections.map(section => {
                             const [dueDate, aids] = section;
                             return (
                                 <React.Fragment key={section[0]}>
@@ -70,6 +117,22 @@ class AssignmentsList extends React.Component {
                         })}
                     </List>
                 </div>
+                <AppBar position="relative" color="primary">
+                    <Toolbar variant="dense">
+                        <FormControl>
+                            <FormControlLabel
+                                control={<Switch color="default" />}
+                                label={
+                                    this.state.showingPastAssignments
+                                        ? 'Showing past assignments'
+                                        : 'Showing upcoming assignments'
+                                }
+                                value={this.state.showingPastAssignments}
+                                onChange={this.togglePastAssignments}
+                            />
+                        </FormControl>
+                    </Toolbar>
+                </AppBar>
                 <Fab
                     className="add-button"
                     color="secondary"
@@ -82,7 +145,7 @@ class AssignmentsList extends React.Component {
     }
 }
 AssignmentsList.propTypes = {
-    sections: PropTypes.arrayOf(PropTypes.array),
+    assignments: PropTypes.arrayOf(PropTypes.object),
     fetchAssignments: PropTypes.func,
     createAssignment: PropTypes.func,
     deleteAssignment: PropTypes.func,
@@ -91,14 +154,9 @@ AssignmentsList.propTypes = {
 
 const mapStateToProps = state => {
     const selectors = getAssignmentsSelectors(state.assignments);
-    const sections = {};
-    for (const assignment of selectors.selectAll()) {
-        if (sections[assignment.dueDate]) sections[assignment.dueDate].push(assignment.aid);
-        else sections[assignment.dueDate] = [assignment.aid];
-    }
 
     return {
-        sections: Object.entries(sections).sort((a, b) => compareDueDates(a[0], b[0])),
+        assignments: selectors.selectAll(),
     };
 };
 
