844b5ad0482cd9d4f89fb781b3cfedf6bfcb2300
jzassick <60803004+jzassick@users.noreply.github.com>
2020-10-29 00:29:44 -0400

    update 404

--- services/frontend/src/pages/404.js
+++ services/frontend/src/pages/404.js
@@ -1,10 +1,6 @@
 import React from 'react';
+import ErrorPage from '../components/404/404-page';
 
 export default function PageNotFound() {
-    return (
-        <div>
-            <h1>Page not found</h1>
-            <p>We cannot seem to find the page you are looking for.</p>
-        </div>
-    );
+    return <ErrorPage />;
 }
