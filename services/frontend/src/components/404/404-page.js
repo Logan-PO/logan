a301a9be639bb84df8ee621460ce54618575c55e
jzassick <60803004+jzassick@users.noreply.github.com>
2020-10-29 00:45:01 -0400

    Update 404-page.js

--- services/frontend/src/components/404/404-page.js
+++ services/frontend/src/components/404/404-page.js
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types';
 import styles from './404.module.scss';
 
 export default class ErrorPage extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+     constructor(props) {
+	 super(props);
+     }
 
-	render() {
-		return (
-		<div className={styles.errorPage}>
-            		<h1 className={styles.number}>404</h1>
-            		<h2 className={styles.found}>Page not Found</h2>
-            		<p className={styles.last}>The page you are looking for might have been removed or is temporarily unavailable.</p>
-        	</div>
-		);
-	}
+     render() {
+	 return (
+	     <div className={styles.errorPage}>
+            	 <h1 className={styles.number}>404</h1>
+            	 <h2 className={styles.found}>Page not Found</h2>
+            	 <p className={styles.last}>The page you are looking for might have been removed or is temporarily unavailable.</p>
+             </div>
+         );
+     }
 }
