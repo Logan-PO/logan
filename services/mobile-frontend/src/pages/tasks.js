c593063570af0e8e413b0215e954003362aa6d27
prao3 <44528013+prao3@users.noreply.github.com>
2020-10-31 17:46:32 -0400

    Adding a place to land from assignments

---
+++ services/mobile-frontend/src/pages/tasks.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Tasks() {
+    return (
+        <View>
+            <Text>Test task page</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
