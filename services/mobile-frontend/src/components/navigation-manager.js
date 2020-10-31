daaaf96f772d96ff4b22012bde8e059ef0a44579
prao3 <44528013+prao3@users.noreply.github.com>
2020-10-31 18:09:18 -0400

    Building out navigation-manager.js

--- services/mobile-frontend/src/components/navigation-manager.js
+++ services/mobile-frontend/src/components/navigation-manager.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Assignments from '../pages/assignments';
+import Tasks from '../pages/tasks';
 
 const Stack = createStackNavigator();
 
 const navigationManager = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator></Stack.Navigator>
+            <Stack.Navigator>
+                <Stack.Screen name={'Assignments'} component={Assignments} options={{ title: 'Assignments' }} />
+                <Stack.Screen name={'Tasks'} component={Tasks} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 };
