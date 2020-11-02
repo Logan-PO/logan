1d557dbd206f0f1b57ea95cee9ba55b755226ae4
Adam Stewart <adamjoesph11@gmail.com>
2020-11-02 14:20:35 -0500

    beginning implementation of overview section cell

--- services/frontend/src/components/overview/overview-section-cell.js
+++ services/frontend/src/components/overview/overview-section-cell.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getScheduleSelectors } from '../../store/schedule';
+import _ from 'lodash';
+import { ListItem, ListItemText } from '@material-ui/core';
+import { CourseLabel } from '../shared/displays';
+import { getSectionSelectors } from '../../store/schedule';
 
 export class OverviewSectionCell extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            section: this.props.selectCourseFromStore(this.props.sid),
+            section: this.props.selectSectionFromStore(this.props.sid),
         };
     }
 
     render() {
-        return <div className="list-cell">Hello</div>;
+        return (
+            <div className="list-cell">
+                <ListItem>
+                    <ListItemText
+                        primary={
+                            <React.Fragment>
+                                <div className="cell-upper-label">
+                                    <CourseLabel cid={this.props.cid} />
+                                </div>
+                                <div>{_.get(this.state, 'section.title')}</div>
+                            </React.Fragment>
+                        }
+                        secondary={_.get(this.state, 'section.location')}
+                    />
+                </ListItem>
+            </div>
+        );
     }
 }
 OverviewSectionCell.propTypes = {
     sid: PropTypes.string,
-    selectCourseFromStore: PropTypes.func,
+    cid: PropTypes.string,
+    tid: PropTypes.string,
+    selectSectionFromStore: PropTypes.func,
 };
 
 const mapStateToProps = state => {
     return {
-        selectCourseFromStore: getScheduleSelectors(state.schedule).baseSelectors.courses.selectById,
+        selectSectionFromStore: getSectionSelectors(state.sections).selectById,
     };
 };
 
