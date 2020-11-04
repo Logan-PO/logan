371bb08314d3d3d3b63cd74dd38de48f1e3a655c
Lucas Popp <lmp122@case.edu>
2020-11-04 14:43:17 -0500

    Create reminders controller and establish schemas

---
+++ services/backend/src/reminders-controller.js
@@ -0,0 +1,49 @@
+const _ = require('lodash');
+const { dynamoUtils } = require('@logan/aws');
+const { v4: uuid } = require('uuid');
+const requestValidator = require('../utils/request-validator');
+
+/**
+ * @typedef {Object} Reminder
+ * @property {string} rid
+ * @property {string} uid
+ * @property {string} entityType Either assignment or task
+ * @property {string} eid The entity ID
+ * @property {string} timestamp When to send the reminder
+ * @property {string} message The content of the reminder
+ */
+
+/**
+ * @typedef DBReminder
+ * @property {string} rid
+ * @property {string} uid
+ * @property {string} et entityType
+ * @property {string} eid
+ * @property {string} ts timestamp
+ * @property {string} msg content
+ */
+
+/**
+ * @param {DBReminder} db
+ * @return {Reminder}
+ */
+function fromDbFormat(db) {
+    return {
+        ..._.pick(db, ['rid', 'uid', 'eid']),
+        entityType: db.et,
+        timestamp: db.ts,
+        message: db.msg,
+    };
+}
+
+/**
+ * @param {Reminder }reminder
+ * @return {DBReminder}
+ */
+function toDbFormat(reminder) {
+
+}
+
+module.exports = {
+    __test_only__: { fromDbFormat, toDbFormat },
+};
