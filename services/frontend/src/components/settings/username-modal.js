ee651f22ab5c865168cac78a1ef51cbea4196cf3
jzassick <60803004+jzassick@users.noreply.github.com>
2020-11-04 18:44:54 -0500

    add settings page modals

---
+++ services/frontend/src/components/settings/username-modal.js
@@ -0,0 +1,78 @@
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Dialog, DialogContent, DialogActions, TextField, Button } from '@material-ui/core';
+import { updateUser } from '../../store/settings';
+import { setLoginStage, fetchSelf } from '../../store/login';
+
+class UsernameModal extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.close = this.close.bind(this);
+        this.updateUser = this.updateUser.bind(this);
+
+        this.state = {
+            user: {
+                uid: _.get(this.props, 'user.uid'),
+                name: _.get(this.props, 'user.name'),
+                email: _.get(this.props, 'user.email'),
+                username: 'empty',
+            },
+        };
+    }
+
+    close() {
+        this.props.onClose();
+    }
+
+    async updateUser() {
+        await this.props.updateUser(this.state.user);
+        this.props.fetchSelf();
+        this.props.onClose();
+    }
+
+    handleChange(prop, e) {
+        const user = this.state.user;
+        user[prop] = e.target.value;
+        this.setState({ user });
+    }
+
+    render() {
+        return (
+            <Dialog open={this.props.open} onClose={this.props.onClose} fullWidth maxWidth="sm">
+                <DialogContent>
+                    <TextField label="New Username" fullWidth onChange={this.handleChange.bind(this, 'username')} />
+                </DialogContent>
+                <DialogActions>
+                    <div>
+                        <Button variant="contained" color="primary" disableElevation onClick={this.updateUser}>
+                            Change
+                        </Button>
+                    </div>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
+
+UsernameModal.propTypes = {
+    user: PropTypes.object,
+    open: PropTypes.bool,
+    onClose: PropTypes.func,
+    updateUser: PropTypes.func,
+    fetchSelf: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+    user: state.login.user,
+});
+
+const mapDispatchToProps = {
+    setLoginStage,
+    updateUser,
+    fetchSelf,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsernameModal);
