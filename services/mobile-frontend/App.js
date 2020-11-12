ea782acc21f0b7315090ea6f8ede80fa17e5be4f
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-12 17:28:15 -0500

    Deleting old files

--- services/mobile-frontend/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NavigationManager from '../mobile-frontend/src/components/navigation-manager';
-
-export default function App() {
-    return <NavigationManager />;
-}
