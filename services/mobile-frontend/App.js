1b4463fed993a6de8991d6b8379984d74c4e4dcb
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-13 10:48:30 -0500

    Trying to use expo to build again

--- services/mobile-frontend/App.js
+++ services/mobile-frontend/App.js
@@ -1,6 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import NavigationManager from '../mobile-frontend/src/components/navigation-manager';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return <NavigationManager />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
