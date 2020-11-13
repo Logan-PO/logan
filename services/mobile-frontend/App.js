e92e0264a9d489c69ec97245bf893a9d1ec3cf46
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-13 10:46:41 -0500

    Revert "Deleting old files"
    This reverts commit ea782acc21f0b7315090ea6f8ede80fa17e5be4f.

---
+++ services/mobile-frontend/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import NavigationManager from '../mobile-frontend/src/components/navigation-manager';
+
+export default function App() {
+    return <NavigationManager />;
+}
