6f4cd72b257845ce198b7aa6553df3580ffa9382
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-13 12:44:46 -0500

    Fixing some linting issues

--- services/mobile-frontend/App.js
+++ services/mobile-frontend/App.js
@@ -2,5 +2,5 @@ import React from 'react';
 import NavigationManager from '../mobile-frontend/src/components/navigation-manager';
 
 export default function App() {
-  return <NavigationManager />;
+    return <NavigationManager />;
 }
