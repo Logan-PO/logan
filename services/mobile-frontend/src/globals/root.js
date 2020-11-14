ebc1da17e27b463e3a82f761174b6a9a1bed3bfa
Lucas Popp <lmp122@case.edu>
2020-11-13 19:17:20 -0500

    Add react native paper to the root

--- services/mobile-frontend/src/globals/root.js
+++ services/mobile-frontend/src/globals/root.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider as ReduxProvider } from 'react-redux';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { store } from '@logan/fe-shared';
 
 class Root extends React.Component {
     render() {
-        return <ReduxProvider store={store}>{this.props.children}</ReduxProvider>;
+        return (
+            <ReduxProvider store={store}>
+                <PaperProvider>{this.props.children}</PaperProvider>
+            </ReduxProvider>
+        );
     }
 }
 
