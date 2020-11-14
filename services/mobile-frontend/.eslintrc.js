905ededb4ce1543771f1f6245b52ef550731fb8f
Lucas Popp <lmp122@case.edu>
2020-11-14 11:02:01 -0500

    Ignore generated folders in mobile frontend

--- services/mobile-frontend/.eslintrc.js
+++ services/mobile-frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['../../.eslintrc.js', 'plugin:react/recommended'],
-    ignorePatterns: ['public/**', '.cache/**'],
+    ignorePatterns: ['android/**', 'ios/**'],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
