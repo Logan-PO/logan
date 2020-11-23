2f1c84f87e86146b3ece5a702ac4ecb80331fe47
Lucas Popp <lmp122@case.edu>
2020-11-23 14:06:41 -0500

    Add UTC translation to the frontend API

---
+++ packages/fe-shared/utils/utc-translation.js
@@ -0,0 +1,56 @@
+const { dateUtils } = require('@logan/core');
+
+function timeToUTC(time) {
+    return dateUtils.formatAsTime(dateUtils.toTime(time).utc());
+}
+
+function timeFromUTC(time) {
+    const utc = dateUtils.toTime(time).utc(true);
+    return dateUtils.formatAsTime(utc.local());
+}
+
+function dateTimeToUTC(ts) {
+    return dateUtils.formatAsDateTime(dateUtils.toDateTime(ts).utc());
+}
+
+function dateTimeFromUTC(ts) {
+    const utc = dateUtils.toDateTime(ts).utc(true);
+    return dateUtils.formatAsDateTime(utc.local());
+}
+
+function sectionFromUTC(original) {
+    const updated = { ...original };
+    updated.startTime = timeFromUTC(original.startTime);
+    updated.endTime = timeFromUTC(original.endTime);
+    return updated;
+}
+
+function sectionToUTC(original) {
+    const updated = { ...original };
+    updated.startTime = timeToUTC(original.startTime);
+    updated.endTime = timeToUTC(original.endTime);
+    return updated;
+}
+
+function reminderFromUTC(original) {
+    const updated = { ...original };
+    updated.timestamp = dateTimeFromUTC(original.timestamp);
+    return updated;
+}
+
+function reminderToUTC(original) {
+    const updated = { ...original };
+    updated.timestamp = dateTimeToUTC(original.timestamp);
+    return updated;
+}
+
+module.exports = {
+    timeToUTC,
+    timeFromUTC,
+    dateTimeToUTC,
+    dateTimeFromUTC,
+    sectionToUTC,
+    sectionFromUTC,
+    reminderToUTC,
+    reminderFromUTC,
+};
