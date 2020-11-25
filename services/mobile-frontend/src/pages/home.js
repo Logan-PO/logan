d993d34728f45d8008ba529569b46eda1d71c9e4
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-25 10:48:21 -0600

    Adding the button to the home page

---
+++ services/mobile-frontend/src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MobileLoginButton from '../components/home/mobile-login-button';
+
+class Home extends React.Component {
+    render() {
+        return <MobileLoginButton />;
+    }
+}
+
+export default Home;
