ce6e74d71d53ff499f3bf394d9c6d62c8d498088
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-25 20:20:46 -0600

    Adding some state change handling and todos in home.js

--- services/mobile-frontend/src/screens/home.js
+++ services/mobile-frontend/src/screens/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { fetchSelf, setLoginStage } from '@logan/fe-shared/store/login';
+import { fetchSelf, LOGIN_STAGE, setLoginStage } from '@logan/fe-shared/store/login';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MobileLoginButton from '../components/home/mobile-login-button';
@@ -15,7 +15,16 @@ class Home extends React.Component {
     navigate to the create users screen
      */
     async componentDidUpdate(prevProps) {
+        //Do nothing if state doesn't update meaningfully
         if (_.isEqual(this.props, prevProps)) return;
+
+        if (this.props.loginStage === LOGIN_STAGE.LOGGED_IN) {
+            await this.props.fetchSelf();
+            this.props.setLoginStage(LOGIN_STAGE.DONE);
+            //TODO: Go to tasks screen
+        } else if (this.props.loginStage === LOGIN_STAGE.CREATE) {
+            //TODO: Go to new user screen
+        }
     }
 
     render() {
