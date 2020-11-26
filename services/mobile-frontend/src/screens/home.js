16012887ac9afbb9d6284656db16d091308c458c
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-25 20:12:47 -0600

    In componentDidUpdate, if nothing important changes, do nothing

--- services/mobile-frontend/src/screens/home.js
+++ services/mobile-frontend/src/screens/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { fetchSelf, setLoginStage } from '@logan/fe-shared/store/login';
 import PropTypes from 'prop-types';
@@ -6,11 +7,16 @@ import { connect } from 'react-redux';
 import MobileLoginButton from '../components/home/mobile-login-button';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     /*
     When the login stage is updated, we want to either navigate to the tasks screen or
     navigate to the create users screen
      */
-    async componentDidUpdate(prevProps) {}
+    async componentDidUpdate(prevProps) {
+        if (_.isEqual(this.props, prevProps)) return;
+    }
 
     render() {
         return (
