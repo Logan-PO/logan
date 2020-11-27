bcaa859beb5fd86add574dc2110133bbf7cb3c6c
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-27 14:58:02 -0600

    Getting and handling new user input

--- services/mobile-frontend/src/components/home/signup-form.js
+++ services/mobile-frontend/src/components/home/signup-form.js
@@ -3,7 +3,8 @@ import _ from 'lodash';
 import { createNewUser, LOGIN_STAGE, setLoginStage } from '@logan/fe-shared/store/login';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Button } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import Typography from '../shared/typography';
 
 class SignUpForm extends React.Component {
@@ -11,13 +12,16 @@ class SignUpForm extends React.Component {
         super(props);
 
         this.state = { name: '', username: '', email: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidUpdate(prevProps) {
         if (_.isEqual(this.props, prevProps)) return;
 
         if (this.props.loginStage !== LOGIN_STAGE.CREATE) {
-            this.props.onClose();
+            this.props.navigation.navigate('Root');
         } else {
             this.setState({
                 name: _.get(this.props, ['meta', 'name'], ''),
@@ -28,7 +32,7 @@ class SignUpForm extends React.Component {
     }
 
     handleChange(prop, e) {
-        this.setState({ [prop]: e.target.value });
+        this.setState({ [prop]: e });
     }
 
     async handleSubmit(e) {
@@ -40,6 +44,28 @@ class SignUpForm extends React.Component {
         return (
             <SafeAreaView>
                 <Typography variant="h6">Create Account</Typography>
+                <TextInput
+                    style={{ paddingHorizontal: 0, flexGrow: 1, backgroundColor: 'none' }}
+                    mode="flat"
+                    label="Name"
+                    value={this.state.name}
+                    onChangeText={this.handleChange.bind(this, 'name')}
+                />
+                <TextInput
+                    style={{ paddingHorizontal: 0, flexGrow: 1, backgroundColor: 'none' }}
+                    mode="flat"
+                    label="Username"
+                    value={this.state.username}
+                    onChangeText={this.handleChange.bind(this, 'username')}
+                />
+                <TextInput
+                    style={{ paddingHorizontal: 0, flexGrow: 1, backgroundColor: 'none' }}
+                    mode="flat"
+                    label="Email"
+                    value={this.state.email}
+                    onChangeText={this.handleChange.bind(this, 'email')}
+                />
+                <Button title="Submit" onPress={this.handleSubmit} />
             </SafeAreaView>
         );
     }
@@ -47,6 +73,7 @@ class SignUpForm extends React.Component {
 
 SignUpForm.propTypes = {
     meta: PropTypes.object,
+    navigation: PropTypes.object,
     loginStage: PropTypes.string,
     setLoginStage: PropTypes.func,
     createNewUser: PropTypes.func,
