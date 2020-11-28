00dbb94f5a23ed2688b9933681209eccb3b9ddf4
Lucas Popp <lmp122@case.edu>
2020-11-27 21:00:40 -0500

    Add condense/uncondense

--- services/mobile-frontend/src/components/overview/overview-section-cell.js
+++ services/mobile-frontend/src/components/overview/overview-section-cell.js
@@ -3,20 +3,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getSectionSelectors } from '@logan/fe-shared/store/schedule';
-import { View } from 'react-native';
+import { View, LayoutAnimation } from 'react-native';
+import SyncComponent from '@logan/fe-shared/components/sync-component';
 import CourseLabel from '../shared/displays/course-label';
 import ListItem from '../shared/list-item';
 import Typography from '../shared/typography';
 import { printSectionTimes } from './scheduling-utils';
 
-class OverviewSectionCell extends React.Component {
+class OverviewSectionCell extends SyncComponent {
     constructor(props) {
         super(props);
 
+        this.toggleCondensed = this.toggleCondensed.bind(this);
+
         this.state = {
             section: this.props.selectSectionFromStore(this.props.sid),
-            condensed: false,
+            condensed: true,
         };
+    }
+
+    async toggleCondensed() {
+        const duration = 100;
+
+        LayoutAnimation.configureNext(
+            LayoutAnimation.create(duration, LayoutAnimation.Types.easeInEaseOut, LayoutAnimation.Properties.opacity)
+        );
+
+        await this.setStateSync({ condensed: !this.state.condensed });
+
+        return new Promise(resolve => setTimeout(resolve, duration));
     }
 
     makeDetailText(title, value) {
@@ -29,20 +44,43 @@ class OverviewSectionCell extends React.Component {
     }
 
     render() {
+        const section = _.get(this.state, 'section.title');
+        const location = _.get(this.state, 'section.location');
+        const instructor = _.get(this.state, 'section.instructor');
+
         return (
             <ListItem
                 leftContent={
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
                         <View style={{ flex: 2 }}>
                             <Typography variant="body2">{printSectionTimes(this.state.section)}</Typography>
                         </View>
                         <View style={{ flex: 3 }}>
                             <CourseLabel cid={_.get(this.state, 'section.cid')} />
+                            {!this.state.condensed && (
+                                <React.Fragment>
+                                    {!_.isEmpty(section) && (
+                                        <Typography variant="body2" color="detail">
+                                            Title: {section}
+                                        </Typography>
+                                    )}
+                                    {!_.isEmpty(location) && (
+                                        <Typography variant="body2" color="detail">
+                                            Location: {location}
+                                        </Typography>
+                                    )}
+                                    {!_.isEmpty(instructor) && (
+                                        <Typography variant="body2" color="detail">
+                                            Instructor: {instructor}
+                                        </Typography>
+                                    )}
+                                </React.Fragment>
+                            )}
                         </View>
                     </View>
                 }
                 contentStyle={{ minHeight: 0, paddingVertical: 10 }}
-                onClick={() => this.setState({ condensed: !this.state.condensed })}
+                onPress={this.toggleCondensed}
             />
         );
     }
