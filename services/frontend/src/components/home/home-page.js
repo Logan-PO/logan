4f760888777c211b4c2062fa1b83dd1bfa7438f5
Lucas Popp <lmp122@case.edu>
2020-11-28 12:53:54 -0500

    Styling!

--- services/frontend/src/components/home/home-page.js
+++ services/frontend/src/components/home/home-page.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link, navigate } from 'gatsby';
-import { Container, Typography, Grid } from '@material-ui/core';
+import { Container, Typography, Grid, colors } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { LOGIN_STAGE, setLoginStage, fetchSelf } from '@logan/fe-shared/store/login';
 import GoogleBtn from './GoogleButton';
@@ -39,43 +39,48 @@ class HomePage extends React.Component {
 
     render() {
         return (
-            <Container className={styles.homePage}>
-                <Grid container direction="column">
-                    <Grid item xs={12}>
-                        <Typography variant="h2">Logan</Typography>
-                        <Typography>
-                            Logan is an intelligent personal organizer, designed with college students in mind.
-                        </Typography>
+            <div
+                className={styles.homePage}
+                style={{ width: '100%', minHeight: '100vh', background: colors.teal[500] }}
+            >
+                <Container>
+                    <Grid container direction="column">
+                        <Grid item xs={12}>
+                            <Typography variant="h2">Logan</Typography>
+                            <Typography>
+                                Logan is an intelligent personal organizer, designed with college students in mind.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="h4">Features</Typography>
+                            <Typography>Feature info here</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <br />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <GoogleBtn />
+                            {this.props.loginStage === LOGIN_STAGE.DONE && (
+                                <React.Fragment>
+                                    <br />
+                                    <br />
+                                    <Link to="/tasks" style={{ color: 'white', textDecoration: 'none' }}>
+                                        Open Logan <ExitToAppIcon fontSize="small" />
+                                    </Link>
+                                </React.Fragment>
+                            )}
+                        </Grid>
+                        <Grid item xs={12}>
+                            <br />
+                            <br />
+                            <Link to="/tutorial" style={{ color: 'white', textDecoration: 'none' }}>
+                                Open Tutorial <ExitToAppIcon fontSize="small" />
+                            </Link>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Typography variant="h4">Features</Typography>
-                        <Typography>Feature info here</Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <br />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <GoogleBtn />
-                        {this.props.loginStage === LOGIN_STAGE.DONE && (
-                            <React.Fragment>
-                                <br />
-                                <br />
-                                <Link to="/tasks">
-                                    Open Logan <ExitToAppIcon fontSize="small" />
-                                </Link>
-                            </React.Fragment>
-                        )}
-                    </Grid>
-                    <Grid item xs={12}>
-                        <br />
-                        <br />
-                        <Link to="/tutorial">
-                            Open Tutorial <ExitToAppIcon fontSize="small" />
-                        </Link>
-                    </Grid>
-                </Grid>
-                <SignUpForm open={this.state.createModalOpen} onClose={this.modalClosed} />
-            </Container>
+                    <SignUpForm open={this.state.createModalOpen} onClose={this.modalClosed} />
+                </Container>
+            </div>
         );
     }
 }
