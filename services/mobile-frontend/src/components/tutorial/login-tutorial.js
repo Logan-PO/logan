400f974f8032ce6abc711468f4a84233c692e3cb
prao3 <44528013+prao3@users.noreply.github.com>
2020-11-29 13:16:52 -0600

    Adding a login tutorial

--- services/mobile-frontend/src/components/tutorial/login-tutorial.js
+++ services/mobile-frontend/src/components/tutorial/login-tutorial.js
@@ -1,47 +1,59 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { View, ScrollView } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Typography from '../shared/typography';
 
 class LoginTutorial extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        let scale = 0.8;
         return (
             <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <View>
-                        <Typography variant="h3">Login Screen</Typography>
-                    </View>
-                    <View style={{ width: '90%', height: '90%' }}>
-                        <Image
-                            style={{ width: '100%', height: '100%' }}
-                            source={require('./images/login_screen.jpg')}
-                        />
-                    </View>
-                    <View>
-                        <Typography variant="body">
-                            This is test, a;okjdhf;lakhf;lokaw;okhfd;dkjfa';skjfl;akjdf;lkajd';lkj;ja;slkfjals;dkj
-                            ;slkdfjgl;skjg;lksjfg;lksjdf;lgkjsl;fg;jlajkdshflakjhdflkajhdfklajhsdlkfjhasldfjhljalkjdfhklajhdfklajhsdfk
-                            lakjhdflkajhdflkahdslkjhalkshjdf;lkajshfdlkajshfvlkjahvja;sjdbfnklajvblkjasbv ljasbhv
-                            as;lkklhavk;jhask;jvblaksjvblkjasbvkjbasvkljbaskljdbkaljshfg;lkoashngbasVasd'p'as;asdfjb
-                        </Typography>
-                    </View>
-                </ScrollView>
+                <View>
+                    <Typography variant="h3">Login</Typography>
+                </View>
+                <View
+                    style={{
+                        width: Dimensions.get('screen').width * scale,
+                        height: Dimensions.get('screen').height * scale,
+                    }}
+                >
+                    <Image
+                        style={{
+                            width: Dimensions.get('screen').width * scale,
+                            height: Dimensions.get('screen').height * scale,
+                        }}
+                        source={require('./images/login_screen.jpg')}
+                    />
+                </View>
+                <View>
+                    <Typography style={{ textAlign: 'center' }} variant="body">
+                        {'\n'}To login with Logan, just press the login button, and follow the on-screen instructions.
+                        If you are a new user, input your name, desired username, and email in the create user page (see
+                        below). Once completed, you should be taken to the Overview page. {'\n'}
+                    </Typography>
+                </View>
+                <View
+                    style={{
+                        width: Dimensions.get('screen').width * scale,
+                        height: Dimensions.get('screen').height * scale,
+                    }}
+                >
+                    <Image
+                        style={{
+                            width: Dimensions.get('screen').width * scale,
+                            height: Dimensions.get('screen').height * scale,
+                        }}
+                        source={require('./images/create_account.jpg')}
+                    />
+                </View>
             </SafeAreaView>
         );
     }
 }
-
-/*
-<View>
-                    <Typography variant="h3">Login Screen</Typography>
-                </View>
-                <View style={{ width: '90%', height: '90%' }}>
-                    <Image style={{ width: '100%', height: '100%' }} source={require('./images/login_screen.jpg')} />
-                </View>
-                <View>
-                    <Typography variant="body">This is test text</Typography>
-                </View>
- */
 
 export default LoginTutorial;
--- services/mobile-frontend/src/components/tutorial/tutorial-screen.js
+++ services/mobile-frontend/src/components/tutorial/tutorial-screen.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Dimensions, ScrollView } from 'react-native';
 import LoginTutorial from './login-tutorial';
 
 class Tutorial extends React.Component {
     render() {
-        return <LoginTutorial />;
+        return (
+            <ScrollView
+                contentContainerStyle={{
+                    height: Dimensions.get('screen').height * 2,
+                    width: Dimensions.get('screen').width,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    paddingBottom: '10%',
+                    paddingTop: '10%',
+                }}
+            >
+                <LoginTutorial />
+            </ScrollView>
+        );
     }
 }
 
