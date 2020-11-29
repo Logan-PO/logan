a5d40625132ea055b3b09acca17266e61127e88d
Lucas Popp <lmp122@case.edu>
2020-11-29 15:59:58 -0500

    Add reminders for assignments

--- services/mobile-frontend/src/components/assignments/assignment-details.js
+++ services/mobile-frontend/src/components/assignments/assignment-details.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import Editor from '@logan/fe-shared/components/editor';
 import { List, FAB } from 'react-native-paper';
 import ViewController from '../shared/view-controller';
+import RemindersList from '../reminders/reminders-list';
 import AssignmentEditor from './assignment-editor';
 import SubtasksList from './subtasks-list';
 
 class AssignmentDetails extends React.Component {
     constructor(props) {
         super(props, { id: 'aid', entity: 'assignment', mobile: true });
+
         this.onUpdate = this.onUpdate.bind(this);
         this.newSubtask = this.newSubtask.bind(this);
+        this.newReminder = this.newReminder.bind(this);
+
+        this.state = {
+            assignment: {},
+            fabOpen: false,
+        };
     }
 
     newSubtask() {
@@ -21,42 +29,63 @@ class AssignmentDetails extends React.Component {
         });
     }
 
+    newReminder() {
+        this.props.navigation.navigate('New Reminder', {
+            screen: 'New Reminder',
+            params: {
+                eid: this.state.assignment.aid,
+                entityType: 'assignment',
+            },
+        });
+    }
+
     onUpdate(assignment) {
         this.setState({ assignment });
     }
 
     render() {
-        //TODO: Generalize this maybe?
         return (
-            <ViewController title="Assignment" navigation={this.props.navigation} route={this.props.route}>
-                <ScrollView keyboardDismissMode="on-drag">
-                    <AssignmentEditor
-                        route={this.props.route}
-                        navigation={this.props.navigation}
-                        mode={Editor.Mode.Edit}
-                        onChange={this.onUpdate}
-                    />
-                    <List.Subheader>Subtasks</List.Subheader>
-                    <SubtasksList
-                        route={this.props.route}
-                        navigation={this.props.navigation}
-                        aid={this.props.route.params.aid}
-                    />
-                </ScrollView>
-                <View>
-                    <FAB
-                        icon="plus"
-                        color="white"
-                        style={{
-                            position: 'absolute',
-                            margin: 16,
-                            bottom: 0,
-                            right: 0,
-                        }}
-                        onPress={this.newSubtask}
-                    />
-                </View>
-            </ViewController>
+            <React.Fragment>
+                <ViewController title="Assignment" navigation={this.props.navigation} route={this.props.route}>
+                    <ScrollView keyboardDismissMode="on-drag">
+                        <AssignmentEditor
+                            route={this.props.route}
+                            navigation={this.props.navigation}
+                            mode={Editor.Mode.Edit}
+                            onChange={this.onUpdate}
+                        />
+                        <List.Subheader>Subtasks</List.Subheader>
+                        <SubtasksList
+                            route={this.props.route}
+                            navigation={this.props.navigation}
+                            aid={this.props.route.params.aid}
+                        />
+                        <RemindersList
+                            eid={this.state.assignment.aid}
+                            navigation={this.props.navigation}
+                            route={this.props.route}
+                        />
+                    </ScrollView>
+                </ViewController>
+                <FAB.Group
+                    open={this.state.fabOpen}
+                    color="white"
+                    icon={this.state.fabOpen ? 'close' : 'plus'}
+                    actions={[
+                        {
+                            icon: 'bell-plus',
+                            label: 'Reminder',
+                            onPress: this.newReminder,
+                        },
+                        {
+                            icon: 'plus',
+                            label: 'Subtask',
+                            onPress: this.newSubtask,
+                        },
+                    ]}
+                    onStateChange={({ open }) => this.setState({ fabOpen: open })}
+                />
+            </React.Fragment>
         );
     }
 }
