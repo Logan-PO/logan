a1f21016cac62cde3f983ffc7a712748b6865047
jzassick <60803004+jzassick@users.noreply.github.com>
2020-12-16 14:16:17 -0500

    Update cascading-deletes.test.js

--- services/backend/src/controllers/cascading-deletes.test.js
+++ services/backend/src/controllers/cascading-deletes.test.js
@@ -474,7 +474,7 @@ describe('Terms', () => {
 
     // Delete one of the terms, and ensure only the other term's entities remain
     it('Successful delete', async () => {
-        await controllers.terms.deleteTerm({ params: basicTerm1, auth: { uid: 'usr123' } }, { json: jsonMock });
+        await controllers.terms.deleteTerm({ pathParameters: { tid: basicTerm1.tid }, auth: { uid: 'usr123' } });
 
         const { Items: termsLeft, Count: termCount } = await dynamoUtils.scan({ TableName: 'terms' });
         expect(termCount).toEqual(1);
