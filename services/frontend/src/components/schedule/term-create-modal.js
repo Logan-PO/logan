1e827a34e7b10b29d8b9a7e4e8cdeba0f84142a8
prao3 <44528013+prao3@users.noreply.github.com>
2020-12-18 18:49:22 -0600

    Adding a dialog to pop up

--- services/frontend/src/components/schedule/term-create-modal.js
+++ services/frontend/src/components/schedule/term-create-modal.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { createTerm } from '@logan/fe-shared/store/schedule';
+import { Dialog, DialogTitle } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-class TermCreateModal extends React.Component {}
+class TermCreateModal extends React.Component {
+    render() {
+        return (
+            <Dialog open={this.props.open} onClose={this.props.onClose} fullWidth={true} maxWidth="xs">
+                <DialogTitle>Create Term</DialogTitle>
+            </Dialog>
+        );
+    }
+}
 
 TermCreateModal.propTypes = {
+    onClose: PropTypes.func,
+    open: PropTypes.bool,
     createTerm: PropTypes.func,
 };
 
