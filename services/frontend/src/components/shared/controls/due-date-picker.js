3f90147ad3dae4417e324052c3f2f900f4edc797
Lucas Popp <lucaspopp0@gmail.com>
2021-01-13 16:19:02 -0500

    Custom due date picker style

--- services/frontend/src/components/shared/controls/due-date-picker.js
+++ services/frontend/src/components/shared/controls/due-date-picker.js
@@ -2,15 +2,26 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { dateUtils } from '@logan/core';
-import { FormControlLabel, RadioGroup, Radio } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import DueDateIcon from '@material-ui/icons/CalendarToday';
 import { DatePicker } from '@material-ui/pickers';
+import Typography from '../typography';
 import InputGroup from './input-group';
+import styles from './due-date-picker.module.scss';
 
 const {
     dayjs,
     constants: { DB_DATE_FORMAT },
 } = dateUtils;
+
+// eslint-disable-next-line react/prop-types
+const Selectable = ({ selected, children, ...rest }) => {
+    return (
+        <Button className={`${styles.selectable} ${selected ? styles.selected : ''}`} variant="outlined" {...rest}>
+            <Typography variant="body2">{children}</Typography>
+        </Button>
+    );
+};
 
 class DueDatePicker extends React.Component {
     constructor(props) {
@@ -61,9 +72,7 @@ class DueDatePicker extends React.Component {
         }
     }
 
-    updateType(e) {
-        const newType = e.target.value;
-
+    updateType(newType) {
         this.setState({ dueDateType: newType });
 
         if (newType === 'date') {
@@ -94,39 +103,67 @@ class DueDatePicker extends React.Component {
                 label="Due Date"
                 icon={DueDateIcon}
                 content={
-                    <RadioGroup
-                        name="dueDateType"
-                        value={_.get(this.state, 'dueDateType', '')}
-                        onChange={this.updateType}
-                    >
-                        <FormControlLabel
-                            value="asap"
-                            label="ASAP"
-                            labelPlacement="end"
-                            control={<Radio color="primary" />}
-                        />
-                        <FormControlLabel
-                            value="eventually"
-                            label="Eventually"
-                            labelPlacement="end"
-                            control={<Radio color="primary" />}
-                        />
-                        <FormControlLabel
-                            value="date"
-                            label={
-                                <DatePicker
-                                    variant="inline"
-                                    disabled={_.get(this.state, 'dueDateType') !== 'date'}
-                                    value={dateValue}
-                                    onChange={this.updateDate}
-                                    color="primary"
-                                    labelFunc={val => (val ? dateUtils.readableDueDate(val) : 'Choose a date…')}
-                                />
-                            }
-                            labelPlacement="end"
-                            control={<Radio color="primary" />}
-                        />
-                    </RadioGroup>
+                    <div className={styles.selectables}>
+                        <Selectable
+                            selected={this.state.dueDateType === 'asap'}
+                            onClick={this.updateType.bind(this, 'asap')}
+                        >
+                            ASAP
+                        </Selectable>
+                        <Selectable
+                            selected={this.state.dueDateType === 'eventually'}
+                            onClick={this.updateType.bind(this, 'eventually')}
+                        >
+                            Eventually
+                        </Selectable>
+                        <Selectable
+                            selected={this.state.dueDateType === 'date'}
+                            onClick={this.updateType.bind(this, 'date')}
+                        >
+                            <DatePicker
+                                variant="inline"
+                                disabled={_.get(this.state, 'dueDateType') !== 'date'}
+                                value={dateValue}
+                                onChange={this.updateDate}
+                                color="primary"
+                                labelFunc={val => (val ? dateUtils.readableDueDate(val) : 'Choose a date…')}
+                            />
+                        </Selectable>
+                    </div>
+                    // <RadioGroup
+                    //     row
+                    //     name="dueDateType"
+                    //     value={_.get(this.state, 'dueDateType', '')}
+                    //     onChange={this.updateType}
+                    // >
+                    //     <FormControlLabel
+                    //         value="asap"
+                    //         label={<Button>ASAP</Button>}
+                    //         labelPlacement="end"
+                    //         control={<Radio color="primary" />}
+                    //     />
+                    //     <FormControlLabel
+                    //         value="eventually"
+                    //         label="Eventually"
+                    //         labelPlacement="end"
+                    //         control={<Radio color="primary" />}
+                    //     />
+                    //     <FormControlLabel
+                    //         value="date"
+                    //         label={
+                    //             <DatePicker
+                    //                 variant="inline"
+                    //                 disabled={_.get(this.state, 'dueDateType') !== 'date'}
+                    //                 value={dateValue}
+                    //                 onChange={this.updateDate}
+                    //                 color="primary"
+                    //                 labelFunc={val => (val ? dateUtils.readableDueDate(val) : 'Choose a date…')}
+                    //             />
+                    //         }
+                    //         labelPlacement="end"
+                    //         control={<Radio color="primary" />}
+                    //     />
+                    // </RadioGroup>
                 }
             />
         );
