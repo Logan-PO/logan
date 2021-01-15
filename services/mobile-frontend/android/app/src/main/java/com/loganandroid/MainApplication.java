4b09691653d9a9721f3dbf553170beacb5ad5c42
Adam Stewart <adamjoesph11@gmail.com>
2021-01-15 15:36:39 -0500

    android version can be built and run but getting auth errors with google since expo was ejected

--- services/mobile-frontend/android/app/src/main/java/com/loganandroid/MainApplication.java
+++ services/mobile-frontend/android/app/src/main/java/com/loganandroid/MainApplication.java
@@ -11,7 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.mobilefrontend.generated.BasePackageList;
+import com.loganandroid.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
