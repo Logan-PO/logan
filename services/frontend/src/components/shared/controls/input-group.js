8b6dd380c32287a3d46f1c657da495d0bbc8ac97
Lucas Popp <lucaspopp0@gmail.com>
2021-01-15 13:47:29 -0500

    Create color picker

--- services/frontend/src/components/shared/controls/input-group.js
+++ services/frontend/src/components/shared/controls/input-group.js
@@ -7,53 +7,57 @@ import styles from './input-group.module.scss';
  * Displays a fancy input with an optional detail label above, and an optional accessory view to the left. Pass
  * emptyAccessory to keep left padding with no accessory view or icon.
  */
-const InputGroup = ({ style, color, accessory, icon, emptyAccessory = false, label, content, ...rest }) => {
-    const hasAccessory = emptyAccessory || accessory || icon;
-    let accessoryContent = accessory;
+// eslint-disable-next-line react/display-name
+const InputGroup = React.forwardRef(
+    ({ style, color, accessory, icon, emptyAccessory = false, label, content, className, ...rest }, ref) => {
+        const hasAccessory = emptyAccessory || accessory || icon;
+        let accessoryContent = accessory;
 
-    if (icon) {
-        const IconComponent = icon;
-        accessoryContent = <IconComponent style={{ fontSize: 22, color: color || '#646464' }} />;
-    }
+        if (icon) {
+            const IconComponent = icon;
+            accessoryContent = <IconComponent style={{ fontSize: 22, color: color || '#646464' }} />;
+        }
 
-    const classNames = [styles.inputGroup];
+        const classNames = [styles.inputGroup];
 
-    if (rest.className) {
-        classNames.push(rest.className);
-    }
+        if (className) classNames.push(className);
 
-    return (
-        <table style={style} className={classNames.join(' ')}>
-            <tbody>
-                {label && (
-                    <tr>
-                        {hasAccessory && <td />}
-                        <td style={{ width: '100%' }}>
-                            <Typography variant="detail-label">{label}</Typography>
-                        </td>
-                    </tr>
-                )}
-                <tr>
-                    {hasAccessory && (
-                        <td>
-                            <div className={styles.accessory}>{accessoryContent}</div>
-                        </td>
+        return (
+            <table style={style} className={classNames.join(' ')} ref={ref} {...rest}>
+                <tbody>
+                    {label && (
+                        <tr>
+                            {hasAccessory && <td />}
+                            <td style={{ width: '100%' }}>
+                                <Typography style={{ userSelect: 'none' }} variant="detail-label">
+                                    {label}
+                                </Typography>
+                            </td>
+                        </tr>
                     )}
-                    <td style={{ width: '100%' }}>{content}</td>
-                </tr>
-            </tbody>
-        </table>
-    );
-};
+                    <tr>
+                        {hasAccessory && (
+                            <td>
+                                <div className={styles.accessory}>{accessoryContent}</div>
+                            </td>
+                        )}
+                        <td style={{ width: '100%' }}>{content}</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+);
 
 InputGroup.propTypes = {
     style: PropTypes.object,
     accessory: PropTypes.node,
     emptyAccessory: PropTypes.bool,
-    label: PropTypes.string,
+    label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
     content: PropTypes.node,
     icon: PropTypes.elementType,
     color: PropTypes.string,
+    className: PropTypes.string,
 };
 
 export default InputGroup;
