4160526d4080a8fa03592bba084e0a723fa98f2b
Lucas Popp <lucaspopp0@gmail.com>
2021-01-21 17:22:55 -0500

    Add custom color and date pickers on web (#152)
    * Create action buttons
    
    * Create TextInput and InputGroup
    
    * Implement custom inputs
    
    * Fix icon colors in input groups
    
    * Update slider style
    
    * Finalize slider margins
    
    * Fix margins
    
    * Custom due date picker style
    
    * Style course picker
    
    * Style related assignment preview
    
    * Add disabled state to picker
    
    * Paddings!
    
    * Switch due date picker order
    
    * Remove comment
    
    * Update controls in the assignment editor
    
    * Fix another cursor jump bug
    
    * Add comments
    
    * Resize "add tag" button
    
    * Update checkbox style
    
    * Create color picker
    
    * Fix dispatch prop
    
    * Use builtin Popover for color picker
    
    * Fix due date error
    
    * Build a basic date picker
    
    * Set up date picker for tasks
    
    * Add basic due date picker for assignments
    
    * Update tab size
    
    * Prem's picker request

--- services/frontend/src/components/shared/controls/color-picker.js
+++ services/frontend/src/components/shared/controls/color-picker.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormControl, InputLabel, Select, MenuItem, colors as muiColors } from '@material-ui/core';
+import Popover from '@material-ui/core/Popover';
+import * as muiColors from '@material-ui/core/colors';
+import Typography from '../typography';
 import styles from './color-picker.module.scss';
+import InputGroup from './input-group';
 
-const allValidColors = _.omit(muiColors, 'common');
+const allValidColors = _.omit(muiColors, ['common']);
 const primaryColors = _.pick(allValidColors, [
     'teal',
     'green',
@@ -22,53 +25,183 @@ const primaryColors = _.pick(allValidColors, [
 ]);
 
 function camelCaseToSentenceCase(str) {
-    return _.upperFirst(_.camelCase(str).replace(/([A-Z])/g, ' $1'));
+    return _.upperFirst(
+        _.camelCase(str)
+            .replace(/([A-Z])/g, ' $1')
+            .toLowerCase()
+    );
+}
+
+function nameForColor(hexValue) {
+    for (const camelCasedName of _.keys(muiColors)) {
+        if (
+            _.values(muiColors[camelCasedName])
+                .map(hex => hex.toLowerCase())
+                .includes(hexValue.toLowerCase())
+        ) {
+            return camelCaseToSentenceCase(camelCasedName);
+        }
+    }
 }
 
 class ColorPicker extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.pickerRef = React.createRef();
+        this.groupRef = React.createRef();
+
+        this.generateItems = this.generateItems.bind(this);
+        this.selectColor = this.selectColor.bind(this);
+        this.openPicker = this.openPicker.bind(this);
+        this.closePicker = this.closePicker.bind(this);
+
+        this.state = {
+            pickerOpen: false,
+            backdropSize: {
+                width: 100,
+                height: 45,
+            },
+        };
+    }
+
+    componentDidMount() {
+        this._updateBackdropSizeIfNecessary();
+    }
+
+    componentDidUpdate() {
+        this._updateBackdropSizeIfNecessary();
+    }
+
+    _updateBackdropSizeIfNecessary() {
+        if (this.groupRef.current) {
+            if (this.state.backdropSize.width !== this.groupRef.current.offsetWidth) {
+                this.setState({
+                    backdropSize: {
+                        width: this.groupRef.current.offsetWidth,
+                        height: this.groupRef.current.offsetHeight,
+                    },
+                });
+            }
+        }
+    }
+
+    selectColor(hex) {
+        if (this.props.onChange) {
+            this.props.onChange({
+                target: {
+                    value: hex,
+                },
+            });
+        }
+    }
+
+    openPicker() {
+        this.setState({ pickerOpen: true });
+    }
+
+    closePicker() {
+        this.setState({ pickerOpen: false });
+    }
+
     generateItems() {
         const colors = this.props.primaryOnly ? primaryColors : allValidColors;
 
-        const items = [];
+        const rows = [];
 
-        if (!this.props.disableNone) {
-            items.push(
-                <MenuItem key="none" value="" className={styles.item}>
-                    None
-                </MenuItem>
+        const chunks = _.chunk(_.entries(colors), 7);
+
+        let key = 0;
+        for (const chunk of chunks) {
+            rows.push(
+                <div key={key++} className={styles.swatchRow}>
+                    {chunk.map(([key, value]) => {
+                        const name = camelCaseToSentenceCase(key);
+                        const color = value[500];
+
+                        const isSelected = this.props.value === color;
+                        const className = `${styles.swatch} ${isSelected ? styles.selected : ''}`;
+
+                        return (
+                            <div
+                                key={color}
+                                title={name}
+                                className={className}
+                                style={{ background: color }}
+                                onClick={isSelected ? undefined : this.selectColor.bind(this, color)}
+                            />
+                        );
+                    })}
+                </div>
             );
         }
 
-        items.push(
-            ..._.entries(colors).map(([key, value]) => {
-                const name = camelCaseToSentenceCase(key);
-                const color = value[500];
-
-                return (
-                    <MenuItem key={name} value={color} className={styles.item}>
-                        <div className={styles.swatch} style={{ background: color }} />
-                        {name}
-                    </MenuItem>
-                );
-            })
-        );
-
-        return items;
+        return rows;
     }
 
     render() {
         return (
-            <FormControl variant={this.props.variant} fullWidth={this.props.fullWidth} disabled={this.props.disabled}>
-                {this.props.label && <InputLabel>{this.props.label}</InputLabel>}
-                <Select
-                    fullWidth={this.props.fullWidth}
-                    autoWidth
-                    value={this.props.value}
-                    onChange={this.props.onChange}
+            <div className={styles.root}>
+                <div className={styles.inputGroupContainer} onClick={this.openPicker}>
+                    <InputGroup
+                        ref={this.groupRef}
+                        label={this.props.label}
+                        color={this.props.value}
+                        content={
+                            <Typography style={{ userSelect: 'none', color: this.props.value }}>
+                                {nameForColor(this.props.value)}
+                            </Typography>
+                        }
+                    />
+                </div>
+                <Popover
+                    open={this.state.pickerOpen}
+                    anchorEl={this.groupRef.current}
+                    classes={{ paper: styles.pickerPaper }}
+                    elevation={0}
+                    onClose={this.closePicker}
                 >
-                    {this.generateItems()}
-                </Select>
-            </FormControl>
+                    <div className={styles.pickerContainer} style={{ color: this.props.value }}>
+                        <div className={styles.backdropContainer} onClick={this.closePicker}>
+                            <div
+                                className={styles.groupBackdrop}
+                                style={{
+                                    width: this.state.backdropSize.width + 12,
+                                    height: this.state.backdropSize.height + 4,
+                                }}
+                            >
+                                <InputGroup
+                                    style={{ position: 'absolute' }}
+                                    label={
+                                        <span style={{ userSelect: 'none', color: 'white' }}>{this.props.label}</span>
+                                    }
+                                    color="white"
+                                    content={
+                                        <Typography style={{ userSelect: 'none', color: 'white' }}>
+                                            {nameForColor(this.props.value)}
+                                        </Typography>
+                                    }
+                                />
+                            </div>
+                            <svg width={4} height={4} style={{ fill: 'currentColor' }}>
+                                <path
+                                    d="M 0 4
+                                   L 0 0
+                                   A 4 4 0 0 0 4 4
+                                   L 0 4"
+                                />
+                            </svg>
+                        </div>
+                        <div
+                            className={styles.pickerContent}
+                            style={{ top: this.state.backdropSize.height + 4 }}
+                            ref={this.pickerRef}
+                        >
+                            <div className={styles.colorsContainer}>{this.generateItems()}</div>
+                        </div>
+                    </div>
+                </Popover>
+            </div>
         );
     }
 }
--- services/frontend/src/components/shared/controls/input-group.js
+++ services/frontend/src/components/shared/controls/input-group.js
@@ -7,76 +7,82 @@ import styles from './input-group.module.scss';
  * Displays a fancy input with an optional detail label above, and an optional accessory view to the left. Pass
  * emptyAccessory to keep left padding with no accessory view or icon.
  */
-const InputGroup = ({
-    style,
-    color,
-    error = false,
-    helperText,
-    accessory,
-    icon,
-    emptyAccessory = false,
-    label,
-    content,
-    ...rest
-}) => {
-    const hasAccessory = emptyAccessory || accessory || icon;
-    let accessoryContent = accessory;
+// eslint-disable-next-line react/display-name
+const InputGroup = React.forwardRef(
+    (
+        {
+            style,
+            color,
+            error = false,
+            helperText,
+            accessory,
+            icon,
+            className,
+            emptyAccessory = false,
+            label,
+            content,
+            ...rest
+        },
+        ref
+    ) => {
+        const hasAccessory = emptyAccessory || accessory || icon;
+        let accessoryContent = accessory;
 
-    if (icon) {
-        const IconComponent = icon;
-        accessoryContent = <IconComponent style={{ fontSize: 22, color: color || '#646464' }} />;
-    }
+        if (icon) {
+            const IconComponent = icon;
+            accessoryContent = <IconComponent style={{ fontSize: 22, color: color || '#646464' }} />;
+        }
 
-    const classNames = [styles.inputGroup];
+        const classNames = [styles.inputGroup];
 
-    if (rest.className) {
-        classNames.push(rest.className);
-    }
+        if (className) classNames.push(className);
 
-    return (
-        <table style={style} className={classNames.join(' ')}>
-            <tbody>
-                {label && (
-                    <tr>
-                        {hasAccessory && <td />}
-                        <td style={{ width: '100%' }}>
-                            <Typography variant="detail-label" color={error ? 'error' : 'textSecondary'}>
-                                {label}
-                            </Typography>
-                        </td>
-                    </tr>
-                )}
-                <tr>
-                    {hasAccessory && (
-                        <td>
-                            <div className={styles.accessory}>{accessoryContent}</div>
-                        </td>
+        return (
+            <table style={style} className={classNames.join(' ')} ref={ref} {...rest}>
+                <tbody>
+                    {label && (
+                        <tr>
+                            {hasAccessory && <td />}
+                            <td style={{ width: '100%' }}>
+                                <Typography variant="detail-label" color={error ? 'error' : 'textSecondary'}>
+                                    {label}
+                                </Typography>
+                            </td>
+                        </tr>
                     )}
-                    <td style={{ width: '100%' }}>{content}</td>
-                </tr>
-                {helperText && (
                     <tr>
-                        {hasAccessory && <td />}
-                        <td style={{ width: '100%' }}>
-                            <Typography variant="detail" color={error ? 'error' : 'textSecondary'}>
-                                {helperText}
-                            </Typography>
-                        </td>
+                        {hasAccessory && (
+                            <td>
+                                <div className={styles.accessory}>{accessoryContent}</div>
+                            </td>
+                        )}
+                        <td style={{ width: '100%' }}>{content}</td>
                     </tr>
-                )}
-            </tbody>
-        </table>
-    );
-};
+                    {helperText && (
+                        <tr>
+                            {hasAccessory && <td />}
+                            <td style={{ width: '100%' }}>
+                                <Typography variant="detail" color={error ? 'error' : 'textSecondary'}>
+                                    {helperText}
+                                </Typography>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+);
 
 InputGroup.propTypes = {
     style: PropTypes.object,
     accessory: PropTypes.node,
     emptyAccessory: PropTypes.bool,
-    label: PropTypes.string,
+    label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
     content: PropTypes.node,
     icon: PropTypes.elementType,
     color: PropTypes.string,
+    className: PropTypes.string,
     error: PropTypes.bool,
     helperText: PropTypes.string,
 };
