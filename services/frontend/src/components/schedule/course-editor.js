7690f58b969484254a07e4f8a438227ce317cfab
Lucas Popp <lucaspopp0@gmail.com>
2021-01-22 18:50:26 -0500

    Restyle course editor inputs

--- services/frontend/src/components/schedule/course-editor.js
+++ services/frontend/src/components/schedule/course-editor.js
@@ -2,11 +2,14 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Grid, Typography, TextField, Breadcrumbs, Divider } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { getScheduleSelectors, updateCourse, updateCourseLocal } from '@logan/fe-shared/store/schedule';
 import Editor from '@logan/fe-shared/components/editor';
 import '../shared/editor.scss';
 import ColorPicker from '../shared/controls/color-picker';
+import TextInput from '../shared/controls/text-input';
+import InputGroup from '../shared/controls/input-group';
+import editorStyles from './page-editor.module.scss';
 
 class CourseEditor extends Editor {
     constructor(props) {
@@ -32,23 +35,13 @@ class CourseEditor extends Editor {
     }
 
     render() {
-        const term = this.props.selectTerm(_.get(this.state.course, 'tid'));
-
         return (
             <div className="editor">
-                <div className="scroll-view">
+                <div className={`scroll-view ${editorStyles.editor}`}>
                     <Grid container spacing={2} direction="column">
                         <Grid item xs={12}>
-                            <Breadcrumbs>
-                                <Typography color="inherit">{_.get(term, 'title')}</Typography>
-                                <Typography color="textPrimary">Edit Course</Typography>
-                            </Breadcrumbs>
-                        </Grid>
-                        <Divider flexItem style={{ margin: '0 -1em' }} />
-                        <Grid item xs={12}>
-                            <TextField
-                                disabled={this.isEmpty()}
-                                label="Title"
+                            <TextInput
+                                variant="big-input"
                                 fullWidth
                                 value={_.get(this.state.course, 'title', '')}
                                 onChange={this.handleChange.bind(this, 'title')}
@@ -57,15 +50,17 @@ class CourseEditor extends Editor {
                         <Grid item xs={12}>
                             <Grid container direction="row" spacing={2}>
                                 <Grid item xs={6}>
-                                    <TextField
-                                        disabled={this.isEmpty()}
+                                    <InputGroup
+                                        style={{ marginBottom: 12 }}
                                         label="Nickname"
-                                        fullWidth
-                                        value={_.get(this.state.course, 'nickname', '')}
-                                        onChange={this.handleChange.bind(this, 'nickname')}
+                                        content={
+                                            <TextInput
+                                                fullWidth
+                                                value={_.get(this.state.course, 'nickname', '')}
+                                                onChange={this.handleChange.bind(this, 'nickname')}
+                                            />
+                                        }
                                     />
-                                </Grid>
-                                <Grid item xs={6}>
                                     <ColorPicker
                                         fullWidth
                                         label="Color"
@@ -73,6 +68,9 @@ class CourseEditor extends Editor {
                                         value={_.get(this.state.course, 'color', '')}
                                         onChange={this.handleChange.bind(this, 'color')}
                                     />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <InputGroup label="Sections" />
                                 </Grid>
                             </Grid>
                         </Grid>
