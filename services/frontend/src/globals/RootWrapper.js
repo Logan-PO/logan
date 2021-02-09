21682327a8dfb4b8ad99e499cb7b643db2c8c3d6
Lucas Popp <lmp122@case.edu>
2021-02-09 14:44:07 -0500

    Remove references to @material-ui/pickers

--- services/frontend/src/globals/RootWrapper.js
+++ services/frontend/src/globals/RootWrapper.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Provider as StoreProvider } from 'react-redux';
 import { CssBaseline } from '@material-ui/core';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DayJsUtils from '@date-io/dayjs';
 import store from '@logan/fe-shared/store';
 import DynamicThemeProvider from './dynamic-theme-provider';
 
 function wrapper({ element }) {
     return (
         <StoreProvider store={store}>
-            <MuiPickersUtilsProvider utils={DayJsUtils}>
-                <DynamicThemeProvider>
-                    <CssBaseline />
-                    {element}
-                </DynamicThemeProvider>
-            </MuiPickersUtilsProvider>
+            <DynamicThemeProvider>
+                <CssBaseline />
+                {element}
+            </DynamicThemeProvider>
         </StoreProvider>
     );
 }
