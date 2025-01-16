4b4f5b9456285d4de089e1099a706febc28a94dd
Lucas Popp <lucaspopp0@gmail.com>
2025-01-16 00:06:00 -0800

    ci: Add infrastructure pipeline (#192)
    Infrastructure pipeline

---
+++ infra/frontend-bucket.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "logan-frontend-prod"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ infra/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.1"
+
+  required_providers {
+      aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.5.0"
+    }
+  }
+    
+  backend "s3" {
+    bucket         = "logan-terraform-state-prod"
+    key            = "prod/terraform.tfstate"
+    region         = "us-west-2"
+  }
+}
